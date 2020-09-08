c954d2c80420fcfac54a051279c96fd5e146a79f
Bryan Figueroa <bryan@parabeac.com>
2020-09-08 17:03:55 -0600

    Linker ready

--- lib/generation/prototyping/pb_prototype_linker_service.dart
+++ lib/generation/prototyping/pb_prototype_linker_service.dart
@@ -1,1 +1,47 @@
+import 'package:parabeac_core/design_logic/artboard.dart';
+import 'package:parabeac_core/generation/prototyping/pb_prototype_aggregation_service.dart';
+import 'package:parabeac_core/generation/prototyping/pb_prototype_storage.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_layout_intermediate_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_visual_intermediate_node.dart';
 
+class PBPrototypeLinkerService {
+  PBPrototypeStorage _prototypeStorage;
+  PBPrototypeAggregationService _aggregationService;
+
+  PBPrototypeLinkerService() {
+    _prototypeStorage = PBPrototypeStorage();
+    _aggregationService = PBPrototypeAggregationService();
+  }
+
+  Future<PBIntermediateNode> linkSymbols(PBIntermediateNode rootNode) async {
+    if (rootNode == null) {
+      return rootNode;
+    }
+
+    var stack = <PBIntermediateNode>[];
+    PBIntermediateNode rootIntermediateNode;
+    stack.add(rootNode);
+
+    while (stack.isNotEmpty) {
+      var currentNode = stack.removeLast();
+      if (currentNode is PBLayoutIntermediateNode) {
+        currentNode.children.forEach(stack.add);
+      } else if (currentNode is PBVisualIntermediateNode &&
+          currentNode.child != null) {
+        stack.add(currentNode.child);
+      }
+
+      if (currentNode is PBArtboard) {
+        await _prototypeStorage.addPageNode(currentNode);
+        _aggregationService.gatherIntermediateNodes(
+            currentNode.prototypeNode, currentNode);
+      } else if (currentNode.prototypeNode != null &&
+          currentNode.prototypeNode.destinationUUID != null &&
+          currentNode.prototypeNode.destinationUUID.isNotEmpty) {
+        await _prototypeStorage.addPrototypeInstance(currentNode);
+        _aggregationService.gatherPrototypeNodes(currentNode.prototypeNode);
+      }
+    }
+  }
+}
