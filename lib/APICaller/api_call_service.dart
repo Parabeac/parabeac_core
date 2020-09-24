81c01528e33bd7dbd9d56b3b69885750ecc1dda9
Bryan Figueroa <bryan@parabeac.com>
2020-09-24 16:19:01 -0600

    Added http2 to do API call for Figma

--- lib/APICaller/api_call_service.dart
+++ lib/APICaller/api_call_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
-import 'package:http/http.dart' as http;
+import 'package:http2/http2.dart';
 
 import 'api_exceptions.dart';
 
@@ -10,37 +10,60 @@ class APICallService {
 
   Future<dynamic> makeAPICall(List<String> args) async {
     var response;
-    try {
-      /// TODO: plug logger in here
-      var tempResponse =
-          await http.get("https://api.figma.com/v1/files/${args[0]}", headers: {
-        'X-Figma-Token': '${args[1]}',
-      });
-      if (tempResponse.statusCode == 400) {
-        var qFix = File('tempJson.json');
-        response = qFix.readAsStringSync().toString();
-      } else {
-        response = _returnResponse(tempResponse);
+    var uri = Uri.parse("${args[0]}");
+
+    var transport = ClientTransportConnection.viaSocket(
+      await SecureSocket.connect(
+        uri.host,
+        uri.port,
+        supportedProtocols: ['h2'],
+      ),
+    );
+
+    var stream = transport.makeRequest(
+      [
+        Header.ascii(':method', 'GET'),
+        Header.ascii(':path', uri.path),
+        Header.ascii(':scheme', uri.scheme),
+        Header.ascii(':authority', uri.host),
+        Header.ascii('x-figma-token', args[1]),
+      ],
+      endStream: true,
+    );
+
+    final buffer = StringBuffer();
+    await for (var message in stream.incomingMessages) {
+      if (message is HeadersStreamMessage) {
+        for (var header in message.headers) {
+          var name = utf8.decode(header.name);
+          var value = utf8.decode(header.value);
+          print('Header: $name: $value');
+        }
+      } else if (message is DataStreamMessage) {
+        // Use [message.bytes] (but respect 'content-encoding' header)
+        buffer.write(utf8.decode(message.bytes));
       }
-    } on SocketException {
-      print('No internet connection.');
     }
-    return json.decode(response);
+    await transport.finish();
+    var map = buffer.toString();
+    response = json.decode(map);
+    return response;
   }
 
-  dynamic _returnResponse(http.Response response) {
-    switch (response.statusCode) {
-      case 200:
-        return response;
-      case 400:
-        throw BadRequestException(response.body.toString());
-      case 401:
-      case 403:
-        throw UnauthorisedException(response.body.toString());
-      case 500:
-      default:
-        throw FetchDataException(
-            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
-    }
-  }
+  /// TODO: Adapt it to the new http2 or remove it
+  // dynamic _returnResponse(http.Response response) {
+  //   switch (response.statusCode) {
+  //     case 200:
+  //       return response;
+  //     case 400:
+  //       throw BadRequestException(response.body.toString());
+  //     case 401:
+  //     case 403:
+  //       throw UnauthorisedException(response.body.toString());
+  //     case 500:
+  //     default:
+  //       throw FetchDataException(
+  //           'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+  //   }
+  // }
 }
