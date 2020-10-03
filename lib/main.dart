f09406df735813653b406ef5d7d26caac04edf95
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-10-03 11:53:12 -0400

    Using Dart's ArgParser to parse arguments

--- lib/main.dart
+++ lib/main.dart
@@ -9,110 +9,132 @@ import 'package:quick_log/quick_log.dart';
 import 'package:sentry/sentry.dart';
 import 'package:uuid/uuid.dart';
 import 'package:http/http.dart' as http;
+import 'package:args/args.dart';
 
 import 'controllers/main_info.dart';
 
-String pathToSketchFile;
 String resultsDirectory = Platform.environment['SENTRY_DSN'] ?? '/temp';
 final SentryClient sentry = SentryClient(dsn: resultsDirectory);
+ArgResults argResults;
 
 void main(List<String> args) async {
   await checkConfigFile();
 
+  //Sentry logging initialization
   MainInfo().sentry = SentryClient(dsn: resultsDirectory);
   var log = Logger('Main');
-
   log.info(args.toString());
 
   MainInfo().cwd = Directory.current;
 
-  var path = '';
-  var projectName = '';
+  ///sets up parser
+  final parser = ArgParser()
+    ..addOption('path',
+        help: 'Path to the design file', valueHelp: 'path', abbr: 'p')
+    ..addOption('out', help: 'The output path', valueHelp: 'path', abbr: 'o')
+    ..addOption(
+      'project-name',
+      help: 'The name of the project',
+      abbr: 'n',
+      defaultsTo: 'temp',
+    )
+    ..addOption(
+      'config-path',
+      help: 'Path of the configuration file',
+      abbr: 'c',
+      defaultsTo: 'lib/configurations/configurations.json',
+    )
+    ..addFlag(
+      'help',
+      help: 'Displays this help information.',
+      abbr: 'h',
+      negatable: false,
+    );
+  ;
+
+  void handleError(String msg) {
+    log.error(msg);
+    exitCode = 2;
+    exit(2);
+  }
+
+  argResults = parser.parse(args);
+
+  if (argResults['help']) {
+    print('''
+  ** PARABEAC HELP **
+${parser.usage}
+    ''');
+    exit(0);
+  }
+
+  String path = argResults['path'];
   var designType = 'sketch';
-  final _helpText = "Usage Options: \n" +
-      "-p \t Path to the sketch File \n" +
-      "-o \t Output Path\n" +
-      "-n \t Name of the project\n" +
-      "-c \t Path of the configuration file\n";
-
-  //Check if no args passed or only -h passed
-  //If arguments is empty or only has -h
-  if (args.length == 0 || args[0] == '-h') {
-    print(_helpText);
-    return;
-  }
-  var configurationPath = 'lib/configurations/configurations.json';
+  var configurationPath = argResults['config-path'];
   var configurationType = 'default';
-  for (var i = 0; i < args.length; i += 2) {
-    switch (args[i]) {
-      case '-p':
-        path = args[i + 1];
-        pathToSketchFile = path;
-        MainInfo().sketchPath = pathToSketchFile;
-        // If outputPath is empty, assume we are outputting to sketch path
-        MainInfo().outputPath ??= getCleanPath(path);
-        if (pathToSketchFile.endsWith('.sketch')) {
-          designType = 'sketch';
-        } else if (pathToSketchFile.endsWith('.fig')) {
-          designType = 'figma';
-        }
-        break;
-      case '-o':
-        MainInfo().outputPath = args[i + 1];
-        break;
-      case '-n':
-        projectName = args[i + 1];
-        break;
-      //  usage -c "default:lib/configurations/configurations.json
-      case '-c':
-        var configSet = args[i + 1].split(':');
-        if (configSet.isNotEmpty) {
-          configurationType = configSet[0];
-        }
-        if (configSet.length >= 2) {
-          // handle configurations
-          configurationPath = configSet[1];
-        }
-        break;
+  String projectName = argResults['project-name'];
+
+  if (path == null) {
+    handleError('Missing required argument: path');
+  } else {
+    if (!await FileSystemEntity.isFile(path)) {
+      handleError('$path is not a file');
     }
-  }
 
-  if (!MainInfo().outputPath.endsWith('/')) {
-    MainInfo().outputPath += '/';
-  }
+    if (path.endsWith('.sketch')) {
+      designType = 'sketch';
+    } else if (path.endsWith('.fig')) {
+      designType = 'figma';
+    }
 
-  if (projectName.isEmpty) {
-    projectName = 'temp';
-  }
+    //  usage -c "default:lib/configurations/configurations.json
+    var configSet = configurationPath.split(':');
+    if (configSet.isNotEmpty) {
+      configurationType = configSet[0];
+    }
+    if (configSet.length >= 2) {
+      // handle configurations
+      configurationPath = configSet[1];
+    }
 
-  MainInfo().projectName = projectName;
+    // Populate `MainInfo()`
+    MainInfo().outputPath = argResults['out'];
+    // If outputPath is empty, assume we are outputting to design file path path
+    MainInfo().outputPath ??= getCleanPath(path);
+    if (!MainInfo().outputPath.endsWith('/')) {
+      MainInfo().outputPath += '/';
+    }
+    MainInfo().sketchPath = path;
+    MainInfo().projectName = projectName;
 
-  // Input
-  var id = InputDesignService(path);
+    // Input
+    var id = InputDesignService(path);
 
-  if (designType == 'sketch') {
-    var process = await Process.start('npm', ['run', 'prod'],
-        workingDirectory: MainInfo().cwd.path + '/SketchAssetConverter');
+    if (designType == 'sketch') {
+      var process = await Process.start('npm', ['run', 'prod'],
+          workingDirectory: MainInfo().cwd.path + '/SketchAssetConverter');
 
-    await for (var event in process.stdout.transform(utf8.decoder)) {
-      if (event.toLowerCase().contains('server is listening on port')) {
-        log.fine('Successfully started Sketch Asset Converter');
-        break;
+      await for (var event in process.stdout.transform(utf8.decoder)) {
+        if (event.toLowerCase().contains('server is listening on port')) {
+          log.fine('Successfully started Sketch Asset Converter');
+          break;
+        }
       }
-    }
 
-    //Retrieving the Sketch PNGs from the design file
-    await Directory('${MainInfo().outputPath}pngs').create(recursive: true);
-    await SketchController().convertSketchFile(
-        pathToSketchFile,
-        MainInfo().outputPath + projectName,
-        configurationPath,
-        configurationType);
-    process.kill();
-  } else if (designType == 'xd') {
-    assert(false, 'We don\'t support Adobe XD.');
-  } else if (designType == 'figma') {
-    assert(false, 'We don\'t support Figma.');
+      //Retrieving the Sketch PNGs from the design file
+      await Directory('${MainInfo().outputPath}pngs').create(recursive: true);
+      await SketchController().convertSketchFile(
+          path,
+          MainInfo().outputPath + projectName,
+          configurationPath,
+          configurationType);
+      process.kill();
+    } else if (designType == 'xd') {
+      assert(false, 'We don\'t support Adobe XD.');
+    } else if (designType == 'figma') {
+      assert(false, 'We don\'t support Figma.');
+    }
+    exitCode = 0;
   }
 }
 
