9b3cd1d7b93e3bbf2b2435d49db5d2070a342e4c
Haardik Dharma <dharmahaardik08@gmail.com>
2020-10-05 12:43:37 +0530

    (fix:#51) Refactored PBParam to PBVariable (#87)
    * refactor PBParam to PBVariable
    
    * Downgraded build_runner to 1.10.0
    
    Co-authored-by: Ivan <42812006+ivan-015@users.noreply.github.com>

--- lib/generation/generators/pb_widget_manager.dart
+++ lib/generation/generators/pb_widget_manager.dart
@@ -1,6 +1,6 @@
 import 'package:parabeac_core/generation/generators/pb_generator.dart';
 import 'package:parabeac_core/generation/generators/pb_page_writer.dart';
-import 'package:parabeac_core/generation/generators/pb_param.dart';
+import 'package:parabeac_core/generation/generators/pb_variable.dart';
 import 'package:parabeac_core/generation/generators/visual-widgets/pb_bitmap_gen.dart';
 import 'package:parabeac_core/generation/generators/visual-widgets/pb_shape_group_gen.dart';
 import 'package:parabeac_core/generation/generators/visual-widgets/pb_spacer_gen.dart';
@@ -8,7 +8,7 @@ import 'package:parabeac_core/generation/generators/visual-widgets/pb_text_gen.d
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_gen_cache.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 
-import 'pb_param.dart';
+import 'pb_variable.dart';
 
 /**
  * The caller of the method `generate()` is going to look at the configurations and decide if the page is
@@ -37,8 +37,8 @@ abstract class PBGenerationManager {
   ///* Keep track of the instance variable a class may have
   /// I think the following three may be only for Flutter (?)
 
-  List<PBParam> constructorVariables = [];
-  List<PBParam> instanceVariables = [];
+  List<PBVariable> constructorVariables = [];
+  List<PBVariable> instanceVariables = [];
   Map<String, String> dependencies = {};
 
   PBGenerationManager(
@@ -52,7 +52,7 @@ abstract class PBGenerationManager {
 
   String getPath(String uuid) => PBGenCache().getPath(uuid);
 
-  void addConstructorVariable(PBParam param);
+  void addConstructorVariable(PBVariable param);
 
-  void addInstanceVariable(PBParam variable);
+  void addInstanceVariable(PBVariable variable);
 }
