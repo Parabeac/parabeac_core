e94766a99ac76e475711816ccf95c794f0b7a8b1
Bryan Figueroa <bryan@parabeac.com>
2020-10-09 19:54:06 -0600

    Added case where scaffold only come after canvas

--- lib/controllers/figma_controller.dart
+++ lib/controllers/figma_controller.dart
@@ -1,5 +1,6 @@
 import 'package:parabeac_core/controllers/controller.dart';
 import 'package:parabeac_core/generation/flutter_project_builder/flutter_project_builder.dart';
+import 'package:parabeac_core/input/figma/entities/layers/frame.dart';
 import 'package:parabeac_core/input/figma/helper/figma_node_tree.dart';
 import 'package:quick_log/quick_log.dart';
 
@@ -16,7 +17,9 @@ class FigmaController extends Controller {
   void convertFile(var jsonFigma, var outputPath, var configurationPath) async {
     configure(configurationPath);
 
-    var figmaNodeTree = generateFigmaTree(jsonFigma, outputPath);
+    var figmaNodeTree = await generateFigmaTree(jsonFigma, outputPath);
+
+    figmaNodeTree = declareScaffolds(figmaNodeTree);
 
     Interpret().init(outputPath);
 
@@ -35,5 +38,16 @@ class FigmaController extends Controller {
       print(e);
       return null;
     }
+  }
+
+  FigmaNodeTree declareScaffolds(FigmaNodeTree tree) {
+    for (var page in tree.pages) {
+      for (var item in page.getPageItems()) {
+        if (item.root is FigmaFrame) {
+          (item.root as FigmaFrame).isScaffold = true;
+        }
+      }
+    }
+    return tree;
   }
 }
