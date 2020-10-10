f9a01ccd2b7b5fa1b75b64363aa0a03817f4d81c
Bryan Figueroa <bryan@parabeac.com>
2020-10-09 19:54:53 -0600

    Frame can also be Groups

--- lib/input/figma/entities/layers/frame.dart
+++ lib/input/figma/entities/layers/frame.dart
@@ -2,23 +2,31 @@ import 'package:json_annotation/json_annotation.dart';
 import 'package:parabeac_core/design_logic/artboard.dart';
 import 'package:parabeac_core/design_logic/color.dart';
 import 'package:parabeac_core/design_logic/group_node.dart';
+import 'package:parabeac_core/design_logic/image.dart';
+import 'package:parabeac_core/design_logic/pb_border.dart';
 import 'package:parabeac_core/input/figma/entities/abstract_figma_node_factory.dart';
 import 'package:parabeac_core/input/figma/entities/layers/figma_node.dart';
+import 'package:parabeac_core/input/figma/entities/layers/group.dart';
 import 'package:parabeac_core/input/figma/entities/style/figma_color.dart';
 import 'package:parabeac_core/input/figma/helper/style_extractor.dart';
 import 'package:parabeac_core/input/sketch/entities/layers/flow.dart';
 import 'package:parabeac_core/input/sketch/entities/style/color.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/inherited_bitmap.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/inherited_container.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/inherited_scaffold.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 import 'package:parabeac_core/input/sketch/entities/objects/frame.dart';
+import 'package:parabeac_core/interpret_and_optimize/value_objects/point.dart';
+import 'package:parabeac_core/input/figma/helper/image_helper.dart'
+    as image_helper;
 
 part 'frame.g.dart';
 
 @JsonSerializable(nullable: true)
 class FigmaFrame extends FigmaNode
     with PBColorMixin
-    implements FigmaNodeFactory, GroupNode, PBArtboard {
+    implements FigmaNodeFactory, GroupNode, PBArtboard, Image {
   @override
   @JsonKey(name: 'absoluteBoundingBox')
   var boundaryRectangle;
@@ -62,6 +70,9 @@ class FigmaFrame extends FigmaNode
   @override
   String type = 'FRAME';
 
+  @JsonKey(ignore: true)
+  bool isScaffold = false;
+
   FigmaFrame({
     name,
     isVisible,
@@ -93,12 +104,16 @@ class FigmaFrame extends FigmaNode
           sharedPluginData,
           UUID: UUID,
         );
+  @JsonKey(ignore: true)
+  List points;
+
+  @JsonKey(name: 'fills')
+  List fillsList;
 
   @override
   FigmaNode createFigmaNode(Map<String, dynamic> json) {
     var node = FigmaFrame.fromJson(json);
     node.style = StyleExtractor().getStyle(json);
-    this.backgroundColor = node.style.backgroundColor;
     return node;
   }
 
@@ -110,11 +125,62 @@ class FigmaFrame extends FigmaNode
   @override
   Future<PBIntermediateNode> interpretNode(PBContext currentContext) {
     /// TODO: change `isHomeScreen` to its actual value
-    return Future.value(InheritedScaffold(
-      this,
-      currentContext: currentContext,
-      name: name,
-      isHomeScreen: false,
-    ));
+    if (isScaffold) {
+      return Future.value(InheritedScaffold(
+        this,
+        currentContext: currentContext,
+        name: name,
+        isHomeScreen: false,
+      ));
+    } else {
+      var tempGroup = Group(
+        name: name,
+        isVisible: isVisible,
+        type: type,
+        pluginData: pluginData,
+        sharedPluginData: sharedPluginData,
+        boundaryRectangle: boundaryRectangle,
+        style: style,
+        fills: fills,
+        strokes: strokes,
+        strokeWeight: strokeWeight,
+        strokeAlign: strokeAlign,
+        cornerRadius: cornerRadius,
+        constraints: constraints,
+        layoutAlign: layoutAlign,
+        size: size,
+        horizontalPadding: horizontalPadding,
+        verticalPadding: verticalPadding,
+        itemSpacing: itemSpacing,
+        flow: null,
+        children: children,
+        UUID: UUID,
+        backgroundColor: backgroundColor,
+      );
+
+      return Future.value(tempGroup.interpretNode(currentContext));
+      // PBBorder border;
+      // for (var b in style?.borders?.reversed ?? []) {
+      //   if (b.isEnabled) {
+      //     border = b;
+      //   }
+      // }
+      // return Future.value(InheritedContainer(
+      //   this,
+      //   Point(boundaryRectangle.x, boundaryRectangle.y),
+      //   Point(boundaryRectangle.x + boundaryRectangle.width,
+      //       boundaryRectangle.y + boundaryRectangle.height),
+      //   currentContext: currentContext,
+      //   borderInfo: {
+      //     'borderRadius': (style != null && style.borderOptions.isEnabled)
+      //         ? points[0]['cornerRadius']
+      //         : null,
+      //     'borderColorHex': border != null ? toHex(border.color) : null
+      //   },
+      // ));
+    }
   }
+
+  @override
+  String imageReference;
 }
