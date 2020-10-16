1c9da4d9ac5167398d057ed7051d76cf8455ff25
Niharikajain2812 <niharikajainmiet@gmail.com>
2020-10-16 13:42:57 +0530

    Update design_shape.dart
    import 'package:json_annotation/json_annotation.dart';

--- lib/design_logic/design_shape.dart
+++ lib/design_logic/design_shape.dart
@@ -1,6 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'package:parabeac_core/design_logic/design_element.dart';
-
+import 'package:json_annotation/json_annotation.dart';
 @JsonSerializable()
 abstract class DesignShape extends DesignElement {
   DesignShape() : super();
