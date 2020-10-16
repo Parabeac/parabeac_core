7fae66ff879e7c18dc4770cac4af7e76d0ae4748
Eddie <eherreraga@miners.utep.edu>
2020-10-15 20:33:10 -0600

    Removed the Sentry code; its going to cause runtime exceptions because users do not have the configuration necessary. Added the figma args to the parabeac.dart file.

--- lib/generation/generators/visual-widgets/pb_flexible_gen.dart
+++ lib/generation/generators/visual-widgets/pb_flexible_gen.dart
@@ -20,10 +20,10 @@ class PBFlexibleGenerator extends PBGenerator {
         buffer.write(
             'child: ${manager.generate(source.child, type: source.builder_type ?? BUILDER_TYPE.BODY)},');
       } catch (e, stackTrace) {
-        MainInfo().sentry.captureException(
-              exception: e,
-              stackTrace: stackTrace,
-            );
+        // MainInfo().sentry.captureException(
+        //       exception: e,
+        //       stackTrace: stackTrace,
+        //     );
         log.error(e.toString());
       }
       buffer.write(')');
