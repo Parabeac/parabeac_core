b4b785c94221ee242cbf6f36ad20f21b827c9ec6
Bryan Figueroa <bryan@parabeac.com>
2020-11-04 16:15:08 -0700

    Solved issue with testing on pubspec.yaml

--- test/lib/output_services/dependency_writer_test.dart
+++ test/lib/output_services/dependency_writer_test.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
-
-import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:parabeac_core/generation/generators/pb_flutter_writer.dart';
 
 void main() {
   group('Dependency writer test', () {
     var writer;
-    // TODO: look for a yaml to modify that will not afect anything
-    var yamlAbsPath = '${Directory.current.path}/temp/pubspec.yaml';
-    setUp(() {
+    var yamlAbsPath =
+        '${Directory.current.path}/test/lib/output_services/tempTest/pubspec.yaml';
+    setUp(() async {
+      Process.runSync('flutter', ['create', 'tempTest'],
+          workingDirectory:
+              '${Directory.current.path}/test/lib/output_services/');
       writer = PBFlutterWriter();
       writer.addDependency('http_parser', '^3.1.4');
       writer.addDependency('shelf_proxy', '^0.1.0+7');
@@ -23,16 +24,11 @@ void main() {
       lineShelf = await readYaml.indexOf('  shelf_proxy: ^0.1.0+7');
       expect(lineHttp >= 0, true);
       expect(lineShelf >= 0, true);
-
-      var writeYaml = File(yamlAbsPath).openWrite(mode: FileMode.write);
-
-      for (var i = 0; i < readYaml.length; ++i) {
-        if (i == lineHttp || i == lineShelf) {
-        } else {
-          writeYaml.writeln(readYaml[i]);
-        }
-      }
-      await writeYaml.flush();
+    });
+    tearDownAll(() {
+      Process.runSync('rm', ['-r', 'tempTest'],
+          workingDirectory:
+              '${Directory.current.path}/test/lib/output_services/');
     });
   });
 }
