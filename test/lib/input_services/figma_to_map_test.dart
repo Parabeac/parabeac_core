586ac55cd49d318ce85dcc438b271b639de3f493
Bryan Figueroa <bryan@parabeac.com>
2020-11-04 17:39:56 -0700

    Added Figma API key as enviromental variable

--- test/lib/input_services/figma_to_map_test.dart
+++ test/lib/input_services/figma_to_map_test.dart
@@ -1,10 +1,10 @@
-import 'package:mockito/mockito.dart';
+import 'dart:io';
 import 'package:parabeac_core/APICaller/api_call_service.dart';
 import 'package:test/test.dart';
 
 void main() async {
   var figmaFileID = 'https://api.figma.com/v1/files/zXXWPWb5wJXd0ImGUjEU1X';
-  var figmaAPIKey = '64522-f0e5502a-d2ce-4708-880b-c294e9cb20ed';
+  var figmaAPIKey = Platform.environment['FIG_API_KEY'];
   group('Checking that Figma is getting interpreted as a Map', () {
     setUp(() {});
     test('Figma to Map', () async {
