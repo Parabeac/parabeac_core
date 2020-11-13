01edab8979326de5ffad83405e130fd0717eb212
Ivan Vigliante <ivan.avr@protonmail.com>
2020-11-13 14:47:24 -0700

    Added skip conditions for when user does not have figma api key

--- test/lib/input_services/input_to_pbdl_test.dart
+++ test/lib/input_services/input_to_pbdl_test.dart
@@ -13,43 +13,47 @@ import 'package:test/test.dart';
 void main() {
   var figmaFileID = 'https://api.figma.com/v1/files/zXXWPWb5wJXd0ImGUjEU1X';
   var figmaAPIKey = Platform.environment['FIG_API_KEY'];
-  group('Input to PBDL test', () {
-    var result;
-    var outputPath;
-    var ids;
-    setUp(() async {
-      outputPath = '${Directory.current.path}/temp/';
-      MainInfo().outputPath = outputPath;
+  group(
+    'Input to PBDL test',
+    () {
+      var result;
+      var outputPath;
+      var ids;
+      setUp(() async {
+        outputPath = '${Directory.current.path}/temp/';
+        MainInfo().outputPath = outputPath;
 
-      /// For Figma input
-      result = await APICallService.makeAPICall(figmaFileID, figmaAPIKey);
+        /// For Figma input
+        result = await APICallService.makeAPICall(figmaFileID, figmaAPIKey);
 
-      /// For Sketch Input
-      ids = InputDesignService(
-          '${Directory.current.path}/test/assets/parabeac_demo_alt.sketch');
-    });
-    test('Figma Test', () async {
-      var figmaNodeTree =
-          await FigmaController().generateFigmaTree(result, outputPath);
+        /// For Sketch Input
+        ids = InputDesignService(
+            '${Directory.current.path}/test/assets/parabeac_demo_alt.sketch');
+      });
+      test('Figma Test', () async {
+        var figmaNodeTree =
+            await FigmaController().generateFigmaTree(result, outputPath);
 
-      expect(figmaNodeTree != null, true);
-      expect(figmaNodeTree is FigmaNodeTree, true);
-      expect(figmaNodeTree.pages.isNotEmpty, true);
-      expect(figmaNodeTree.pages[0] is FigmaPage, true);
-    });
-    test('Sketch Test', () {
-      var sketchNodeTree = SketchController().generateSketchNodeTree(
-          ids, ids.metaFileJson['pagesAndArtboards'], outputPath);
+        expect(figmaNodeTree != null, true);
+        expect(figmaNodeTree is FigmaNodeTree, true);
+        expect(figmaNodeTree.pages.isNotEmpty, true);
+        expect(figmaNodeTree.pages[0] is FigmaPage, true);
+      });
+      test('Sketch Test', () {
+        var sketchNodeTree = SketchController().generateSketchNodeTree(
+            ids, ids.metaFileJson['pagesAndArtboards'], outputPath);
 
-      expect(sketchNodeTree != null, true);
-      expect(sketchNodeTree is SketchNodeTree, true);
-      expect(sketchNodeTree.pages.isNotEmpty, true);
-      expect(sketchNodeTree.pages[0] is SketchPage, true);
-    });
-    tearDownAll(() {
-      Process.runSync(
-          '${Directory.current.path}/lib/generation/helperScripts/shell-proxy.sh',
-          ['rm -rf ${outputPath}']);
-    });
-  });
+        expect(sketchNodeTree != null, true);
+        expect(sketchNodeTree is SketchNodeTree, true);
+        expect(sketchNodeTree.pages.isNotEmpty, true);
+        expect(sketchNodeTree.pages[0] is SketchPage, true);
+      });
+      tearDownAll(() {
+        Process.runSync(
+            '${Directory.current.path}/lib/generation/helperScripts/shell-proxy.sh',
+            ['rm -rf ${outputPath}']);
+      });
+    },
+    skip: !Platform.environment.containsKey('FIG_API_KEY'),
+  );
 }
