b121dcf7e82920d709a8d5a3b4112e9aeed44052
Ivan Vigliante <ivan.avr@protonmail.com>
2020-11-13 14:47:24 -0700

    Added skip conditions for when user does not have figma api key

--- test/lib/input_services/figma_to_map_test.dart
+++ test/lib/input_services/figma_to_map_test.dart
@@ -5,17 +5,21 @@ import 'package:test/test.dart';
 void main() async {
   var figmaFileID = 'https://api.figma.com/v1/files/zXXWPWb5wJXd0ImGUjEU1X';
   var figmaAPIKey = Platform.environment['FIG_API_KEY'];
-  group('Checking that Figma is getting interpreted as a Map', () {
-    setUp(() {});
-    test('Figma to Map', () async {
-      var result = await APICallService.makeAPICall(figmaFileID, figmaAPIKey);
-      expect(result is Map, true);
-      expect(result.isNotEmpty, true);
-      expect(result.containsKey('document'), true);
-      expect(result['document'].isNotEmpty, true);
-      expect(result.containsKey('components'), true);
-      expect(result['document']['type'], 'DOCUMENT');
-      expect(result['document']['children'].isNotEmpty, true);
-    });
-  });
+  group(
+    'Checking that Figma is getting interpreted as a Map',
+    () {
+      setUp(() {});
+      test('Figma to Map', () async {
+        var result = await APICallService.makeAPICall(figmaFileID, figmaAPIKey);
+        expect(result is Map, true);
+        expect(result.isNotEmpty, true);
+        expect(result.containsKey('document'), true);
+        expect(result['document'].isNotEmpty, true);
+        expect(result.containsKey('components'), true);
+        expect(result['document']['type'], 'DOCUMENT');
+        expect(result['document']['children'].isNotEmpty, true);
+      });
+    },
+    skip: !Platform.environment.containsKey('FIG_API_KEY'),
+  );
 }
