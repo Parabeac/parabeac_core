8a8982ebfdf8c0bd7abedb9d0785fac38102a1d8
ivanhuerta20 <ivanhuerta20@gmail.com>
2020-11-25 14:31:32 -0700

    Updated PBDestHolder to take in PBContext to fix a bug where alignment values were incorrect

--- lib/generation/prototyping/pb_dest_holder.dart
+++ lib/generation/prototyping/pb_dest_holder.dart
@@ -1,15 +1,16 @@
 import 'package:parabeac_core/generation/prototyping/pb_prototype_gen.dart';
 import 'package:parabeac_core/generation/prototyping/pb_prototype_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
 import 'package:parabeac_core/interpret_and_optimize/value_objects/point.dart';
 
 class PBDestHolder extends PBIntermediateNode {
-
   PrototypeNode pNode;
 
-  PBDestHolder(
-      Point topLeftCorner, Point bottomRightCorner, String UUID, this.pNode)
-      : super(topLeftCorner, bottomRightCorner, UUID, '') {
+  PBDestHolder(Point topLeftCorner, Point bottomRightCorner, String UUID,
+      this.pNode, PBContext currentContext)
+      : super(topLeftCorner, bottomRightCorner, UUID, '',
+            currentContext: currentContext) {
     generator = PBPrototypeGenerator(pNode);
   }
 
