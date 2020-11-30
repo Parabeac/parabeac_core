03fd7bdb852a6d89e9cab434338ab558e403c53c
Ivan Vigliante <ivan.avr@protonmail.com>
2020-11-29 20:34:28 -0500

    Changed uuid of variation to a getter that fetches from its intermediatenode

--- lib/interpret_and_optimize/state_management/intermediate_variation.dart
+++ lib/interpret_and_optimize/state_management/intermediate_variation.dart
@@ -1,8 +1,7 @@
-import 'package:parabeac_core/design_logic/design_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 
 class IntermediateVariation {
-  String uuid;
+  String get UUID => node.UUID;
   PBIntermediateNode node;
-  IntermediateVariation(this.uuid, this.node);
+  IntermediateVariation(this.node);
 }
