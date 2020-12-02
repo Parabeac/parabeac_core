5a2af16fab91eb2be90be57cfb2eca046f11750e
ivanhuerta20 <ivanhuerta20@gmail.com>
2020-12-02 11:53:47 -0700

    Added sudo code & notes around functions needed for stateful management

--- lib/generation/generators/state_management/stateful_management.dart
+++ lib/generation/generators/state_management/stateful_management.dart
@@ -2,6 +2,7 @@ import 'package:parabeac_core/generation/generators/pb_generation_manager.dart';
 import 'package:parabeac_core/generation/generators/pb_variable.dart';
 import 'package:parabeac_core/generation/generators/state_management/state_management_config.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/pb_gen_cache.dart';
 
 class StatefulManagement extends StateManagementConfig {
   @override
@@ -9,10 +10,16 @@ class StatefulManagement extends StateManagementConfig {
     /// Add All States as Stateless Widget classes to a Views folder in the same directory as the current class being written in.
     /// If Views folder doesn't exist, inject the folder then begin writing the Stateless Widgets.
     for (var i = 0; i < node.auxiliaryData.stateGraph.states.length; i++) {
+      /// Doesn't have to be implemented this way but you may get the idea. T
+      /// he function adds the node & generation of the node to the relative directory 'Views'.
+      /// If Views didn't exist create the folder.
       // manager.pageWriter.addNodeToDirectory(directory: "Views", node.auxiliaryData.stateGraph.states[i].node);
     }
     var nameOfDefaultNode = _getNameOfNode(node);
+
+    /// Same as the for loop expect this will take care of the default node
     // manager.pageWriter.addNodeToDirectory(directory: "Views", node);
+    /// Lastly, we will need the import for the default node to inject into the parent class.
     // manager.addImport('${directory} + ${nameOfDefaultNode}.dart');
 
     var variable = PBVariable(
@@ -24,7 +31,7 @@ class StatefulManagement extends StateManagementConfig {
 
     manager.addInstanceVariable(variable);
 
-    /* return variable.toString(); */
+    return '${variable.variableName},';
   }
 
   String _getNameOfNode(PBIntermediateNode node) {
