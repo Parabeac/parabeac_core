462171ff9ba260206baae4877fd8ede9de44322b
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-12-19 15:01:17 -0500

    renamed variables for code readability

--- lib/controllers/figma_controller.dart
+++ lib/controllers/figma_controller.dart
@@ -14,20 +14,20 @@ class FigmaController extends Controller {
   FigmaController();
 
   @override
-  void convertFile(var jsonFigma, var outputPath, var configurationPath,
+  void convertFile(var jsonFigma, var projectName, var configurationPath,
       var configType) async {
     configure(configurationPath, configType);
 
-    var figmaNodeTree = await generateFigmaTree(jsonFigma, outputPath);
+    var figmaNodeTree = await generateFigmaTree(jsonFigma, projectName);
 
     figmaNodeTree = declareScaffolds(figmaNodeTree);
 
-    Interpret().init(outputPath);
+    Interpret().init(projectName);
 
     var mainTree = await Interpret().interpretAndOptimize(figmaNodeTree);
 
     var fpb =
-        FlutterProjectBuilder(projectName: outputPath, mainTree: mainTree);
+        FlutterProjectBuilder(projectName: projectName, mainTree: mainTree);
 
     fpb.convertToFlutterProject();
   }
