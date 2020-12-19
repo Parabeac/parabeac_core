fefa1a87b8b0bf0d24d95b26513588cd1a455034
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-12-19 14:53:51 -0500

    Prevent shell scripts from executing on windows

--- parabeac.dart
+++ parabeac.dart
@@ -71,33 +71,41 @@ ${parser.usage}
 
   _basePath = io.Directory.current.path;
 
-  var install = await Process.start('bash', [
-    '${Directory.current.path}/pb-scripts/install.sh',
-  ]);
+//Prevent shell scripts from executing on windows
+  if (_os != 'WIN') {
+    var install = await Process.start(
+      'bash',
+      [
+        '${Directory.current.path}/pb-scripts/install.sh',
+      ],
+      runInShell: true,
+    );
 
-  await stdout.addStream(install.stdout);
-  await stderr.addStream(install.stderr);
-  var exitCode = await install.exitCode;
-  if (exitCode != 0) {
-    print('install.sh finished with exit code $exitCode');
-  }
+    await stdout.addStream(install.stdout);
+    await stderr.addStream(install.stderr);
+    var exitCode = await install.exitCode;
+    if (exitCode != 0) {
+      print('install.sh finished with exit code $exitCode');
+    }
 
-  /// To Download and merge the plugins on the codebase
-  var result = await Process.start(
-    'bash',
-    [
-      '${Directory.current.path}/pb-scripts/merge-plugins.sh',
-      '$url',
-      '$sKey',
-      '$key',
-    ],
-  );
+    /// To Download and merge the plugins on the codebase
+    var result = await Process.start(
+      'bash',
+      [
+        '${Directory.current.path}/pb-scripts/merge-plugins.sh',
+        '$url',
+        '$sKey',
+        '$key',
+      ],
+      runInShell: true,
+    );
 
-  await for (var event in result.stdout.transform(utf8.decoder)) {
-    print(event);
-  }
-  await for (var event in result.stderr.transform(utf8.decoder)) {
-    print(event);
+    await for (var event in result.stdout.transform(utf8.decoder)) {
+      print(event);
+    }
+    await for (var event in result.stderr.transform(utf8.decoder)) {
+      print(event);
+    }
   }
 
   /// To run parabeac-core
