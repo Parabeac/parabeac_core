632747e056a0cab6405af1b4e5cc9bf711e45abe
ivanhuerta20 <ivanhuerta20@gmail.com>
2020-12-29 18:41:09 -0700

    Fixed pascalCase & random constructor generation call in StateLessTemplateStrategy

--- lib/generation/generators/value_objects/template_strategy/stateless_template_strategy.dart
+++ lib/generation/generators/value_objects/template_strategy/stateless_template_strategy.dart
@@ -1,21 +1,21 @@
 import 'package:parabeac_core/generation/generators/pb_generation_manager.dart';
 import 'package:parabeac_core/generation/generators/value_objects/template_strategy/pb_template_strategy.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
+import 'package:recase/recase.dart';
 
 class StatelessTemplateStrategy extends TemplateStrategy {
   @override
   String generateTemplate(PBIntermediateNode node, PBGenerationManager manager,
       {args}) {
     var widgetName = retrieveNodeName(node);
-    var constructorName = '_$widgetName';
     var returnStatement = manager.generate(node.child);
+
     return '''
 ${manager.generateImports()}
 
-class ${widgetName} extends StatelessWidget{
-  const ${widgetName}({Key key}) : super(key : key);
+class ${widgetName.pascalCase} extends StatelessWidget{
+  const ${widgetName.pascalCase}({Key key}) : super(key : key);
   ${manager.generateGlobalVariables()}
-  ${manager.generateConstructor(constructorName)}
 
   @override
   Widget build(BuildContext context){
