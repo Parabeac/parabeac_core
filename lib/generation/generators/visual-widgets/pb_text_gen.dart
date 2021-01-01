2fd3267045a4e8953942740f93c6acdb99f6c934
Ivan <ivan.avr@protonmail.com>
2021-01-01 15:20:29 -0700

    Added TextStyle generate as override property

--- lib/generation/generators/visual-widgets/pb_text_gen.dart
+++ lib/generation/generators/visual-widgets/pb_text_gen.dart
@@ -27,11 +27,8 @@ class PBTextGen extends PBGenerator with PBColorMixin {
             .write(('\'${source.text?.replaceAll('\n', ' ') ?? ''}\'') + ',\n');
       }
       buffer.write('style: ');
-      if (source is PBSharedMasterNode) {
-        var ovrName = SN_UUIDtoVarName[source.UUID + '_textStyle'];
-        if (ovrName != null) {
-          buffer.write('${ovrName} ?? ');
-        }
+      if(SN_UUIDtoVarName.containsKey('${source.UUID}_textStyle')){
+        buffer.write(SN_UUIDtoVarName[source.UUID + '_textStyle']);
       }
 
       buffer.write('TextStyle(\n');
