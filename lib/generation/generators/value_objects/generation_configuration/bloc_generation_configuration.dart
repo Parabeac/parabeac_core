de9eca99b61d1270f4d157a87821e585e317d688
Ivan Vigliante <ivan.avr@protonmail.com>
2021-01-15 20:05:40 -0500

    Added pageWriter to bloc generation

--- lib/generation/generators/value_objects/generation_configuration/bloc_generation_configuration.dart
+++ lib/generation/generators/value_objects/generation_configuration/bloc_generation_configuration.dart
@@ -2,15 +2,13 @@ import 'package:parabeac_core/generation/generators/middleware/state_management/
 import 'package:parabeac_core/generation/generators/value_objects/file_structure_strategy.dart/flutter_file_structure_strategy.dart';
 import 'package:parabeac_core/generation/generators/value_objects/generation_configuration/pb_generation_configuration.dart';
 
-import '../../pb_flutter_writer.dart';
-
 class BLoCGenerationConfiguration extends GenerationConfiguration {
   BLoCGenerationConfiguration();
 
   @override
   Future<void> setUpConfiguration() async {
     generationManager.fileStrategy = FlutterFileStructureStrategy(
-        intermediateTree.projectAbsPath, PBFlutterWriter(), intermediateTree);
+        intermediateTree.projectAbsPath, pageWriter, intermediateTree);
     registerMiddleware(BLoCMiddleware(generationManager));
     fileStructureStrategy = generationManager.fileStrategy;
     logger.info('Setting up the directories');
