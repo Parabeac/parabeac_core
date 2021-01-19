941a61a3e895cc5aa16da939eb74a80471097d3a
Ivan Vigliante <ivan.avr@protonmail.com>
2021-01-19 00:21:12 -0500

    Added more detail to PBTraversalAdapterWriter

--- lib/generation/generators/writers/pb_traversal_adapter_writer.dart
+++ lib/generation/generators/writers/pb_traversal_adapter_writer.dart
@@ -3,7 +3,12 @@ import 'package:parabeac_core/generation/generators/writers/pb_page_writer.dart'
 /// Adapter used to traverse trees using generation
 /// without actually writing to the tree itself.
 ///
-/// TODO: add more detail to purpose of empty methods.
+/// As seen below, the methods overriden do nothing
+/// and simply return. This is because this Adapter's
+/// purpose allows use of the generation phase without
+/// writing any files, which is useful for gathering and
+/// writing information to nodes without outputting 
+/// any code.
 class PBTraversalAdapterWriter extends PBPageWriter {
   @override
   void addDependency(String packageName, String version) {
