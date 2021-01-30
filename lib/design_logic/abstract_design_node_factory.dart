c701274343aa997e46cc4c0adb1166f321599441
Bryan Figueroa <bryan@parabeac.com>
2021-01-30 14:37:13 -0700

    Setup facotry for design nodes

--- lib/design_logic/abstract_design_node_factory.dart
+++ lib/design_logic/abstract_design_node_factory.dart
@@ -1,7 +1,28 @@
 import 'design_node.dart';
 
 class AbstractDesignNodeFactory {
-  static DesignNode getDesignNode(Map<String, dynamic> json) {
-    // TODO: implement cake factory
+  static final String DESIGN_CLASS_KEY = 'pbdfType';
+
+  static final List<DesignNodeFactory> _designNodes = [
+    // TODO: add classes
+  ];
+
+  AbstractDesignNodeFactory();
+
+  static DesignNode getFigmaNode(Map<String, dynamic> json) {
+    var className = json[DESIGN_CLASS_KEY];
+    if (className != null) {
+      for (var designNode in _designNodes) {
+        if (designNode.type == className) {
+          return designNode.createDesignNode(json);
+        }
+      }
+    }
+    return null;
   }
+}
+
+abstract class DesignNodeFactory {
+  String type;
+  DesignNode createDesignNode(Map<String, dynamic> json);
 }
