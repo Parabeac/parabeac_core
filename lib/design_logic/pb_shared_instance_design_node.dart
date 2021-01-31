9e4ea357b83a33ebb18e3d8bac80b41e5d428da0
Bryan Figueroa <bryan@parabeac.com>
2021-01-31 11:07:17 -0700

    Implemented missing fromPBDF

--- lib/design_logic/pb_shared_instance_design_node.dart
+++ lib/design_logic/pb_shared_instance_design_node.dart
@@ -1,4 +1,6 @@
 import 'package:parabeac_core/design_logic/design_node.dart';
+import 'package:parabeac_core/input/sketch/entities/objects/frame.dart';
+import 'package:parabeac_core/input/sketch/entities/objects/override_value.dart';
 
 import 'abstract_design_node_factory.dart';
 
@@ -14,7 +16,30 @@ class PBSharedInstanceDesignNode extends DesignNode
       boundaryRectangle,
       String type,
       style,
-      prototypeNode})
+      prototypeNode,
+      exportOptions,
+      booleanOperation,
+      bool isFixedToViewport,
+      bool isFlippedVertical,
+      bool isFlippedHorizontal,
+      bool isLocked,
+      layerListExpandedType,
+      bool nameIsFixed,
+      resizingConstraint,
+      resizingType,
+      num rotation,
+      sharedStyleID,
+      bool shouldBreakMaskChain,
+      bool hasClippingMask,
+      int clippingMaskMode,
+      userInfo,
+      bool maintainScrollPosition,
+      List overrideValues,
+      num scale,
+      String symbolID,
+      num verticalSpacing,
+      num horizontalSpacing,
+      pbdfType})
       : super(UUID, name, isVisible, boundaryRectangle, type, style,
             prototypeNode);
 
@@ -25,7 +50,43 @@ class PBSharedInstanceDesignNode extends DesignNode
   DesignNode createDesignNode(Map<String, dynamic> json) => fromPBDF(json);
 
   DesignNode fromPBDF(Map<String, dynamic> json) {
-    // TODO implement
-    return null;
+    return PBSharedInstanceDesignNode(
+      UUID: json['id'] as String,
+      booleanOperation: json['booleanOperation'],
+      exportOptions: json['exportOptions'],
+      boundaryRectangle: json['absoluteBoundingBox'] == null
+          ? null
+          : Frame.fromJson(json['absoluteBoundingBox'] as Map<String, dynamic>),
+      isFixedToViewport: json['isFixedToViewport'] as bool,
+      isFlippedHorizontal: json['isFlippedHorizontal'] as bool,
+      isFlippedVertical: json['isFlippedVertical'] as bool,
+      isLocked: json['isLocked'] as bool,
+      isVisible: json['visible'] as bool,
+      layerListExpandedType: json['layerListExpandedType'],
+      name: json['name'] as String,
+      nameIsFixed: json['nameIsFixed'] as bool,
+      resizingConstraint: json['resizingConstraint'],
+      resizingType: json['resizingType'],
+      rotation: json['rotation'] as num,
+      sharedStyleID: json['sharedStyleID'],
+      shouldBreakMaskChain: json['shouldBreakMaskChain'] as bool,
+      hasClippingMask: json['hasClippingMask'] as bool,
+      clippingMaskMode: json['clippingMaskMode'] as int,
+      userInfo: json['userInfo'],
+      maintainScrollPosition: json['maintainScrollPosition'] as bool,
+      overrideValues: (json['overrideValues'] as List)
+          ?.map((e) => e == null
+              ? null
+              : OverridableValue.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      scale: (json['scale'] as num)?.toDouble(),
+      symbolID: json['symbolID'] as String,
+      verticalSpacing: (json['verticalSpacing'] as num)?.toDouble(),
+      horizontalSpacing: (json['horizontalSpacing'] as num)?.toDouble(),
+      type: json['type'] as String,
+      pbdfType: json['pbdfType'],
+    )
+      ..prototypeNodeUUID = json['prototypeNodeUUID'] as String
+      ..parameters = json['parameters'] as List;
   }
 }
--- lib/design_logic/pb_shared_master_node.dart
+++ lib/design_logic/pb_shared_master_node.dart
@@ -1,8 +1,12 @@
 import 'package:parabeac_core/design_logic/design_node.dart';
+import 'package:parabeac_core/design_logic/group_node.dart';
+import 'package:parabeac_core/input/sketch/entities/objects/frame.dart';
+import 'package:parabeac_core/input/sketch/entities/objects/override_property.dart';
 
 import 'abstract_design_node_factory.dart';
 
-class PBSharedMasterDesignNode extends DesignNode implements DesignNodeFactory {
+class PBSharedMasterDesignNode extends DesignNode
+    implements DesignNodeFactory, GroupNode {
   String symbolID;
   List overriadableProperties;
 
@@ -13,7 +17,41 @@ class PBSharedMasterDesignNode extends DesignNode implements DesignNodeFactory {
       boundaryRectangle,
       String type,
       style,
-      prototypeNode})
+      prototypeNode,
+      bool hasClickThrough,
+      groupLayout,
+      booleanOperation,
+      exportOptions,
+      isFixedToViewport,
+      isFlippedHorizontal,
+      isFlippedVertical,
+      isLocked,
+      layerListExpandedType,
+      pbdfType,
+      presetDictionary,
+      List<OverridableProperty> overrideProperties,
+      bool allowsOverrides,
+      nameIsFixed,
+      resizingConstraint,
+      resizingType,
+      horizontalRulerData,
+      bool hasBackgroundColor,
+      rotation,
+      sharedStyleID,
+      shouldBreakMaskChain,
+      hasClippingMask,
+      clippingMaskMode,
+      userInfo,
+      maintainScrollPosition,
+      bool includeBackgroundColorInExport,
+      int changeIdentifier,
+      String symbolID,
+      bool includeBackgroundColorInInstance,
+      verticalRulerData,
+      bool resizesContent,
+      bool includeInCloudUpload,
+      bool isFlowHome,
+      List parameters})
       : super(UUID, name, isVisible, boundaryRectangle, type, style,
             prototypeNode);
 
@@ -24,7 +62,65 @@ class PBSharedMasterDesignNode extends DesignNode implements DesignNodeFactory {
   DesignNode createDesignNode(Map<String, dynamic> json) => fromPBDF(json);
 
   DesignNode fromPBDF(Map<String, dynamic> json) {
-    // TODO implement
-    return null;
+    var node = PBSharedMasterDesignNode(
+      hasClickThrough: json['hasClickThrough'] as bool,
+      groupLayout: json['groupLayout'],
+      UUID: json['id'] as String,
+      booleanOperation: json['booleanOperation'],
+      exportOptions: json['exportOptions'],
+      boundaryRectangle: json['absoluteBoundingBox'] == null
+          ? null
+          : Frame.fromJson(json['absoluteBoundingBox'] as Map<String, dynamic>),
+      isFixedToViewport: json['isFixedToViewport'],
+      isFlippedHorizontal: json['isFlippedHorizontal'],
+      isFlippedVertical: json['isFlippedVertical'],
+      isLocked: json['isLocked'],
+      isVisible: json['visible'],
+      layerListExpandedType: json['layerListExpandedType'],
+      name: json['name'],
+      nameIsFixed: json['nameIsFixed'],
+      resizingConstraint: json['resizingConstraint'],
+      resizingType: json['resizingType'],
+      rotation: json['rotation'],
+      sharedStyleID: json['sharedStyleID'],
+      shouldBreakMaskChain: json['shouldBreakMaskChain'],
+      hasClippingMask: json['hasClippingMask'],
+      clippingMaskMode: json['clippingMaskMode'],
+      userInfo: json['userInfo'],
+      maintainScrollPosition: json['maintainScrollPosition'],
+      hasBackgroundColor: json['hasBackgroundColor'] as bool,
+      horizontalRulerData: json['horizontalRulerData'],
+      includeBackgroundColorInExport:
+          json['includeBackgroundColorInExport'] as bool,
+      includeInCloudUpload: json['includeInCloudUpload'] as bool,
+      isFlowHome: json['isFlowHome'] as bool,
+      resizesContent: json['resizesContent'] as bool,
+      verticalRulerData: json['verticalRulerData'],
+      includeBackgroundColorInInstance:
+          json['includeBackgroundColorInInstance'] as bool,
+      symbolID: json['symbolID'] as String,
+      changeIdentifier: json['changeIdentifier'] as int,
+      allowsOverrides: json['allowsOverrides'] as bool,
+      overrideProperties: (json['overrideProperties'] as List)
+          ?.map((e) => e == null
+              ? null
+              : OverridableProperty.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      presetDictionary: json['presetDictionary'],
+      type: json['type'] as String,
+      pbdfType: json['pbdfType'],
+      parameters: json['parameters'] as List,
+    )..prototypeNodeUUID = json['prototypeNodeUUID'] as String;
+
+    if (json.containsKey('children')) {
+      if (json['children'] != null) {
+        node.children
+            .add(DesignNode.fromPBDF(json['children'] as Map<String, dynamic>));
+      }
+    }
+    return node;
   }
+
+  @override
+  List children = [];
 }
