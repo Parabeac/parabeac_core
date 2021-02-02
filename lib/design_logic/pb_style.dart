f9f44116e892376dd31d6d5561b3a8017b8a54a8
Bryan Figueroa <bryan@parabeac.com>
2021-02-01 19:21:02 -0700

    Added this. to needed fields

--- lib/design_logic/pb_style.dart
+++ lib/design_logic/pb_style.dart
@@ -3,8 +3,10 @@ import 'package:parabeac_core/design_logic/pb_fill.dart';
 import 'package:parabeac_core/design_logic/pb_text_style.dart';
 import 'package:parabeac_core/design_logic/pb_border.dart';
 import 'package:parabeac_core/design_logic/pb_border_options.dart';
+import 'package:parabeac_core/input/figma/entities/style/figma_style.dart';
+import 'package:parabeac_core/input/sketch/entities/style/style.dart';
 
-abstract class PBStyle {
+class PBStyle {
   PBColor backgroundColor;
   List<PBFill> fills;
   List<PBBorder> borders;
@@ -12,5 +14,14 @@ abstract class PBStyle {
   PBTextStyle textStyle;
 
   PBStyle({this.fills, this.backgroundColor});
-  Map<String, dynamic> toJson();
+
+  // toPBDF() {}
+
+  factory PBStyle.fromPBDF(Map<String, dynamic> json) {
+    if (json.containsKey('_class')) {
+      return Style.fromJson(json);
+    } else {
+      return FigmaStyle.fromJson(json);
+    }
+  }
 }
