6368a113c8139e3b65778e6f3515aa43a563e379
Ivan Vigliante <ivan.avr@protonmail.com>
2021-02-22 19:16:07 -0500

    Modified generator text for AutoSizeText feature.

--- test/lib/generation/generator_test.dart
+++ test/lib/generation/generator_test.dart
@@ -1,9 +1,13 @@
 import 'package:parabeac_core/generation/generators/attribute-helper/pb_generator_context.dart';
 import 'package:parabeac_core/generation/generators/pb_flutter_generator.dart';
+import 'package:parabeac_core/generation/generators/util/pb_generation_project_data.dart';
+import 'package:parabeac_core/generation/generators/util/pb_generation_view_data.dart';
 import 'package:parabeac_core/generation/generators/visual-widgets/pb_container_gen.dart';
 import 'package:parabeac_core/generation/generators/visual-widgets/pb_text_gen.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/inherited_container.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/inherited_text.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/pb_project.dart';
 import 'package:parabeac_core/interpret_and_optimize/state_management/intermediate_auxillary_data.dart';
 import 'package:parabeac_core/interpret_and_optimize/value_objects/point.dart';
 import 'package:test/test.dart';
@@ -19,21 +23,48 @@ class MockContext extends Mock implements GeneratorContext {}
 
 class MockData extends Mock implements IntermediateAuxiliaryData {}
 
+class MockPBContext extends Mock implements PBContext {}
+
+class MockProject extends Mock implements PBProject {}
+
+class MockGenProjectData extends Mock implements PBGenerationProjectData {}
+
+class MockPBGenViewData extends Mock implements PBGenerationViewData {}
+
 void main() {
   group('Generator test', () {
     var mockManager, mockGenerator, mockTextGenerator;
     var mockInheritedContainer, mockInheritedText;
     var mockContext, mockData;
+    var mockPBContext;
+    var mockProject;
+    var mockGenProjectData;
+    var mockGenViewData;
     setUp(() {
       mockInheritedContainer = MockInheritedContainer();
       mockInheritedText = MockInheritedText();
       mockContext = MockContext();
       mockData = MockData();
+      mockPBContext = MockPBContext();
+      mockProject = MockProject();
+      mockGenProjectData = MockGenProjectData();
+      mockGenViewData = MockPBGenViewData();
 
       when(mockInheritedContainer.child).thenReturn(mockInheritedText);
 
       when((mockInheritedContainer as MockInheritedContainer).auxiliaryData)
           .thenReturn(mockData);
+
+      when((mockInheritedText as MockInheritedText).currentContext)
+          .thenReturn(mockPBContext);
+
+      when((mockPBContext as MockPBContext).project).thenReturn(mockProject);
+
+      when((mockProject as MockProject).genProjectData)
+          .thenReturn(mockGenProjectData);
+
+      when((mockInheritedText as MockInheritedText).managerData)
+          .thenReturn(mockGenViewData);
 
       when(mockInheritedContainer.topLeftCorner).thenReturn(Point(0, 0));
 
@@ -65,7 +96,7 @@ void main() {
       expect(result is String, true);
 
       // Do not modify
-      expect(result, '''Container(child: Text(
+      expect(result, '''Container(child: AutoSizeText(
 'Test Text',
 style: TextStyle(
 ),
