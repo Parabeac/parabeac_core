44e566d93debc5b9d539695218dc03a5e0746552
Ivan Vigliante <ivan.avr@protonmail.com>
2021-03-26 11:15:35 -0600

    Removed global variables from stateful middleware Now these global vars generate inside the build method of the widget

--- lib/generation/generators/middleware/state_management/stateful_middleware.dart
+++ lib/generation/generators/middleware/state_management/stateful_middleware.dart
@@ -1,14 +1,10 @@
 import 'package:parabeac_core/generation/generators/middleware/middleware.dart';
-import 'package:parabeac_core/generation/generators/middleware/state_management/utils/middleware_utils.dart';
 import 'package:parabeac_core/generation/generators/pb_generation_manager.dart';
 import 'package:parabeac_core/generation/generators/value_objects/file_structure_strategy.dart/flutter_file_structure_strategy.dart';
-import 'package:parabeac_core/generation/generators/value_objects/generator_adapter.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/pb_shared_instance.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_symbol_storage.dart';
 import 'package:recase/recase.dart';
-
-import '../../pb_variable.dart';
 
 class StatefulMiddleware extends Middleware {
   StatefulMiddleware(PBGenerationManager generationManager)
@@ -16,15 +12,10 @@ class StatefulMiddleware extends Middleware {
 
   @override
   Future<PBIntermediateNode> applyMiddleware(PBIntermediateNode node) async {
-    var managerData = node.managerData;
     var fileStrategy = node.currentContext.project.fileStructureStrategy
         as FlutterFileStructureStrategy;
 
     if (node is PBSharedInstanceIntermediateNode) {
-      managerData.addGlobalVariable(await _getVariables(node));
-      if (node.generator is! StringGeneratorAdapter) {
-        node.generator = StringGeneratorAdapter(node.name.snakeCase);
-      }
       addImportToCache(node.SYMBOL_ID, getImportPath(node, fileStrategy));
       return node;
     }
@@ -45,25 +36,6 @@ class StatefulMiddleware extends Middleware {
     });
 
     return node;
-  }
-
-  Future<PBVariable> _getVariables(
-      PBSharedInstanceIntermediateNode node) async {
-    var symbolMaster =
-        PBSymbolStorage().getSharedMasterNodeBySymbolID(node.SYMBOL_ID);
-
-    var watcherName = getVariableName(node.name.snakeCase);
-
-    var tempVar = PBVariable(
-      watcherName,
-      'var ',
-      true,
-      node.functionCallName == symbolMaster.name
-          ? MiddlewareUtils.wrapOnLayout('${symbolMaster.name.pascalCase}')
-          : null,
-    );
-
-    return tempVar;
   }
 
   String getImportPath(PBSharedInstanceIntermediateNode node, fileStrategy) {
