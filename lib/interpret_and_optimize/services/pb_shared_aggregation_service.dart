024bc3c6ed9b66b54e57d63020cd0406a5039d12
Ivan Vigliante <ivan.avr@protonmail.com>
2021-03-29 20:18:49 -0600

    Added method to help get the last UUID for override properties

--- lib/interpret_and_optimize/services/pb_shared_aggregation_service.dart
+++ lib/interpret_and_optimize/services/pb_shared_aggregation_service.dart
@@ -47,11 +47,10 @@ class PBSharedInterAggregationService {
   void gatherSharedParameters(
       PBSharedMasterNode sharedMasterNode, PBIntermediateNode rootChildNode) {
     for (var prop in sharedMasterNode.overridableProperties) {
+      var targetUUID = _findLastOf(prop?.UUID, '/');
       prop.value = PBIntermediateNodeSearcherService.searchNodeByUUID(
-          rootChildNode, prop?.UUID);
-      if (prop.value == null) {
-        log.warning('UUID: ${prop.UUID} not found in searchNodeByUUID');
-      } else if (prop.type == PBSharedInstanceIntermediateNode) {
+          rootChildNode, targetUUID);
+      if (prop.type == PBSharedInstanceIntermediateNode) {
         ///if the [PBSharedMasterNode] contains [PBSharedInstanceIntermediateNode] as parameters
         ///then its going gather the information of its [PBSharedMasterNode].
         gatherSharedValues(prop.value);
@@ -91,7 +90,8 @@ class PBSharedInterAggregationService {
           instanceIntermediateNode.sharedParamValues.map((v) {
         for (var symParam in masterNode.overridableProperties) {
           if (symParam.propertyName == v.overrideName) {
-            return PBSharedParameterValue(symParam.type, v.value, symParam.UUID, symParam.propertyName);
+            return PBSharedParameterValue(
+                symParam.type, v.value, symParam.UUID, symParam.propertyName);
           }
         }
         return null;
@@ -106,4 +106,13 @@ class PBSharedInterAggregationService {
 
   PBSharedMasterNode _searchMasterNode(String masterUUID) =>
       _symbolStorage.getSharedMasterNodeBySymbolID(masterUUID);
+
+  /// Method that splits `target` according to `delimeter`
+  /// and returns the last entry in the list.
+  String _findLastOf(String target, String delimeter) {
+    if (target == null || delimeter == null) {
+      return '';
+    }
+    return target.split(delimeter).last;
+  }
 }
