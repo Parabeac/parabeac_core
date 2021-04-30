23062f87b077767fca85b7683aef8c99a713b9bb
Bryan Figueroa <bryan@parabeac.com>
2021-04-30 17:33:16 -0600

    Implemented cleanTreeParity to avoid duplicated names

--- lib/interpret_and_optimize/helpers/pb_project.dart
+++ lib/interpret_and_optimize/helpers/pb_project.dart
@@ -25,4 +25,41 @@ class PBProject {
     _genProjectData = PBGenerationProjectData();
     _fileStructureStrategy = fileStructureStrategy;
   }
+
+  // Pass through the forest once checking all trees
+  // if two or more trees have the same name and same
+  // orientation, a `_n` counter is added at the end
+  // of their name
+  void cleanTreeParity() {
+    forest.sort((a, b) => a.rootNode.name.compareTo(b.rootNode.name));
+    var currentCounter = 0;
+    var nextCounter = currentCounter + 1;
+    var nameCounter = 1;
+
+    while (nextCounter < forest.length) {
+      var currentTree = forest[currentCounter];
+      var nextTree = forest[nextCounter];
+      if (currentTree.rootNode.name.compareTo(nextTree.rootNode.name) == 0) {
+        if (currentTree.data.orientation == nextTree.data.orientation) {
+          forest[nextCounter].rootNode.name =
+              forest[nextCounter].rootNode.name + '_$nameCounter';
+
+          nameCounter++;
+          nextCounter++;
+        } else {
+          currentCounter++;
+          if (currentCounter >= nextCounter) {
+            nextCounter++;
+          }
+        }
+      } else {
+        currentCounter++;
+        if (currentCounter >= nextCounter) {
+          nextCounter++;
+        }
+        nameCounter = 1;
+      }
+    }
+    print('hello');
+  }
 }
