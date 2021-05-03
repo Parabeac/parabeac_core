37bd2351f1ee35896eaf7515dbcf69d09ca15a2a
Ivan Vigliante <ivan.avr@protonmail.com>
2021-05-03 17:55:05 -0600

    Create AddConstantCommand unit test.

---
+++ test/lib/generation/commands/add_constant_command_test.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:mockito/mockito.dart';
+import 'package:parabeac_core/generation/generators/value_objects/file_structure_strategy/commands/add_constant_command.dart';
+import 'package:parabeac_core/generation/generators/value_objects/file_structure_strategy/commands/file_structure_command.dart';
+import 'package:parabeac_core/generation/generators/value_objects/file_structure_strategy/pb_file_structure_strategy.dart';
+import 'package:parabeac_core/generation/generators/writers/pb_flutter_writer.dart';
+import 'package:test/test.dart';
+
+class MockFSStrategy extends Mock implements FileStructureStrategy {}
+
+void main() {
+  final path = '${Directory.current.path}/test/lib/generation/commands/';
+  group('Add Constant Command', () {
+    var strategy = MockFSStrategy();
+    FileStructureCommand const1;
+    FileStructureCommand const2;
+    setUp(() {
+      // Create temporary directory to output files
+      Process.runSync('mkdir', ['tmptst'], workingDirectory: path);
+
+      const1 = AddConstantCommand('c1', 'String', '\'test\'');
+      const2 = AddConstantCommand('c2', 'int', '20');
+      when(strategy.GENERATED_PROJECT_PATH).thenReturn('${path}tmptst/');
+      when(strategy.pageWriter).thenReturn(PBFlutterWriter());
+    });
+    test('Testing Adding Constants To Project', () {
+      final constPath = '${path}tmptst/lib/constants/constants.dart';
+      const1.write(strategy);
+      const2.write(strategy);
+      expect(File(constPath).existsSync(), true);
+      var constFile = File(constPath).readAsStringSync();
+      expect(constFile.contains('const String c1 = \'test\''), true);
+      expect(constFile.contains('const int c2 = 20'), true);
+    });
+    tearDownAll(() {
+      Process.runSync('rm', ['-r', 'tmptst'], workingDirectory: path);
+    });
+  });
+}
