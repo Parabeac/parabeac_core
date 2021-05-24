6bd763c9f52c99442678e67c16e34df21d635c3c
Ivan Vigliante <ivan.avr@protonmail.com>
2021-05-24 10:37:47 -0600

    Remove generation of `providers` folder

--- lib/generation/generators/value_objects/file_structure_strategy.dart/provider_file_structure_strategy.dart
+++ lib/generation/generators/value_objects/file_structure_strategy.dart/provider_file_structure_strategy.dart
@@ -5,15 +5,12 @@ import 'package:parabeac_core/generation/generators/value_objects/file_structure
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_project.dart';
 
 class ProviderFileStructureStrategy extends FileStructureStrategy {
-  final RELATIVE_PROVIDER_PATH = 'lib/providers/';
   final RELATIVE_MODEL_PATH = 'lib/models/';
-  var _providersPath;
   var _modelsPath;
 
   ProviderFileStructureStrategy(
       String genProjectPath, PBPageWriter pageWriter, PBProject pbProject)
       : super(genProjectPath, pageWriter, pbProject) {
-    _providersPath = '${genProjectPath}${RELATIVE_PROVIDER_PATH}';
     _modelsPath = '${genProjectPath}${RELATIVE_MODEL_PATH}';
   }
 
@@ -27,7 +24,6 @@ class ProviderFileStructureStrategy extends FileStructureStrategy {
   }
 
   Future<void> _generateMissingDirectories() async {
-    Directory(_providersPath).createSync(recursive: true);
     Directory(_modelsPath).createSync(recursive: true);
   }
 
