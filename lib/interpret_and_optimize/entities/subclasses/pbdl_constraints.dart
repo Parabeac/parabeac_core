47dffe9529c423109f5413d8be676556bf440760
ivanhuerta20 <ivanhuerta20@gmail.com>
2021-05-30 00:27:56 -0600

    Added Sketch BitField to PBDL Mapping

--- lib/interpret_and_optimize/entities/subclasses/pbdl_constraints.dart
+++ lib/interpret_and_optimize/entities/subclasses/pbdl_constraints.dart
@@ -1,5 +1,13 @@
 /// Named PBDL in anticipation of the refactor where PBDL becomes the design standard.
+/// Each property must be set, potentially use https://pub.dev/packages/meta to make these named parameters required.
 class PBDLConstraints {
+  PBDLConstraints(
+      {this.pinLeft,
+      this.pinRight,
+      this.pinTop,
+      this.pinBottom,
+      this.fixedHeight,
+      this.fixedWidth});
   bool pinLeft;
   bool pinRight;
   bool pinTop;
