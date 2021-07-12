4f11ea7429a3c99e07d384f34d690544c9eba335
Eddie Herrera <eherreraga@miners.utep.edu>
2021-07-11 21:26:23 -0600

    BUGFIX utf8 decoding as packets are comming in, instead of decoding each message individually, I delegated that as a transform functionwith the utf8 decoder.BUGFIX utf8 decoding as packets are comming in, instead of decoding each message individually, I delegated that as a transform functionwith the utf8 decoder.

--- lib/input/figma/helper/api_call_service.dart
+++ lib/input/figma/helper/api_call_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:async';
 import 'dart:io';
 import 'package:http2/http2.dart';
+import 'package:parabeac_core/controllers/main_info.dart';
 import 'package:quick_log/quick_log.dart';
 
 import 'api_exceptions.dart';
@@ -9,10 +10,10 @@ import 'api_exceptions.dart';
 class APICallService {
   APICallService();
   static var log = Logger('API Call Service');
+  static String HTTP_STATUS_KEY = ':status';
 
   /// Makes a GET call to figma using `url` and `token`
   static Future<dynamic> makeAPICall(String url, String token) async {
-    var response;
     var uri = Uri.parse(url);
 
     /// The following request must be done using http2. The reason is
@@ -38,28 +39,38 @@ class APICallService {
         ],
         endStream: true,
       );
+      var broadcast = stream.incomingMessages.asBroadcastStream();
 
-      final buffer = StringBuffer();
-      await for (var message in stream.incomingMessages) {
-        if (message is HeadersStreamMessage) {
-          for (var header in message.headers) {
-            var name = utf8.decode(header.name);
-            var value = utf8.decode(header.value);
-            // print('Header: $name: $value');
-            if (name == ':status') {
-              _returnResponse(int.parse(value));
-              break;
-            }
-          }
-        } else if (message is DataStreamMessage) {
-          // Use [message.bytes] (but respect 'content-encoding' header)
-          buffer.write(utf8.decode(message.bytes));
-        }
+      HeadersStreamMessage header = await broadcast.firstWhere(
+          (event) => event is HeadersStreamMessage,
+          orElse: () => null);
+
+      /// Fetching the status code of the request
+      if (header != null) {
+        var header_status = header.headers.firstWhere(
+            (header) => utf8.decode(header.name) == HTTP_STATUS_KEY);
+        var status_code = int.parse(utf8.decode(header_status.value));
+        _returnResponse(status_code);
+      } else {
+        _returnResponse(500);
       }
+
+      /// Decoding the data coming in from the API
+      var decoded_data = await broadcast
+          .where((event) => event is DataStreamMessage)
+          .map((event) => (event as DataStreamMessage).bytes)
+          .transform(utf8.decoder)
+          .join()
+          .then((jsonString) => json.decode(jsonString))
+          .onError((error, stackTrace) {
+        MainInfo().sentry.captureException(exception: error);
+        log.error(error.toString());
+
+        _returnResponse(500);
+      });
+
       await transport.finish();
-      var map = buffer.toString();
-      response = json.decode(map);
-      return response;
+      return decoded_data;
     } catch (e) {
       print(e);
     }
@@ -72,20 +83,20 @@ class APICallService {
         // log.debug('API call went successfully : ${status}');
         break;
       case 400:
-        log.error('BadRequestException : ${status}');
+        log.error('BadRequestException : $status');
         throw BadRequestException();
         break;
       case 401:
       case 403:
-        log.error('UnauthorizedException : ${status}');
+        log.error('UnauthorizedException : $status');
         throw UnauthorisedException();
         break;
       case 500:
       default:
         log.error(
-            'Error occured while Communication with Server with StatusCode : ${status}');
+            'Error occured while Communication with Server with StatusCode : $status');
         throw FetchDataException(
-            'Error occured while Communication with Server with StatusCode : ${status}');
+            'Error occured while Communication with Server with StatusCode : $status');
         break;
     }
   }
