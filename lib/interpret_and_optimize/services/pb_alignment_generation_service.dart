bb10399abdc0a7b7eee07fb0aebf76bb0c1f357b
Eddie Herrera <eherreraga@miners.utep.edu>
2021-07-18 12:15:53 -0600

    REFACTORED traversal on align service to leverage the traversal method on intermediate tree

--- lib/interpret_and_optimize/services/pb_alignment_generation_service.dart
+++ lib/interpret_and_optimize/services/pb_alignment_generation_service.dart
@@ -1,6 +1,5 @@
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_layout_intermediate_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_visual_intermediate_node.dart';
-import 'package:parabeac_core/interpret_and_optimize/helpers/layer_tuple.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_intermediate_node_tree.dart';
 import 'package:parabeac_core/interpret_and_optimize/services/pb_generation_service.dart';
@@ -20,7 +19,6 @@ class PBAlignGenerationService implements AITHandler {
 
   /// Should find all layout nodes
   Future<PBIntermediateTree> addAlignmentToLayouts(PBIntermediateTree tree, PBContext context) {
-    currentContext = context;
     var originalRoot = tree.rootNode;
     if (originalRoot == null) {
       log.warning(
@@ -28,34 +26,17 @@ class PBAlignGenerationService implements AITHandler {
       return null;
     }
 
-    var queue = <LayerTuple>[];
-    queue.add(LayerTuple([originalRoot], null));
-    while (queue.isNotEmpty) {
-      var currentLayer = queue.removeAt(0);
-
-      for (var currentIntermediateNode in currentLayer.nodeLayer) {
-        if (currentIntermediateNode is PBVisualIntermediateNode) {
-          currentIntermediateNode.alignChild();
-        } else if (currentIntermediateNode is PBLayoutIntermediateNode) {
-          currentIntermediateNode.alignChildren();
-        }
-        if (currentIntermediateNode == null) {
-          continue;
-        }
-
-        currentIntermediateNode.attributes.forEach((attribute) {
-          attribute.attributeNodes.forEach((node) {
-            queue.add(LayerTuple([node], currentIntermediateNode));
-          });
-        });
+    tree.forEach((node) {
+      if(node is PBVisualIntermediateNode){
+        node.alignChild();
       }
-    }
-    tree.rootNode = originalRoot;
+      if(node is PBLayoutIntermediateNode){
+        // TODO: convert to using aling() on each node.
+        node.alignChildren();
+      }
+     });
     return Future.value(tree);
   }
-
-  @override
-  PBContext currentContext;
 
   @override
   Future<PBIntermediateTree> handleTree(PBContext context, PBIntermediateTree tree) {
