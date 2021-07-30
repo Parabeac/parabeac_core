ecb6fa1e951685e58f6af7e10a778b3361e50cf0
Bryan Figueroa <bryan@parabeac.com>
2021-07-30 12:34:43 -0600

    Corrected images name

--- lib/generation/generators/visual-widgets/pb_bitmap_gen.dart
+++ lib/generation/generators/visual-widgets/pb_bitmap_gen.dart
@@ -1,8 +1,10 @@
+import 'package:parabeac_core/controllers/main_info.dart';
 import 'package:parabeac_core/generation/generators/attribute-helper/pb_size_helper.dart';
 import 'package:parabeac_core/generation/generators/pb_generator.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/inherited_bitmap.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
+import 'package:path/path.dart' as p;
 
 class PBBitmapGenerator extends PBGenerator {
   var _sizehelper;
@@ -21,8 +23,14 @@ class PBBitmapGenerator extends PBGenerator {
     // } else if (SN_UUIDtoVarName.containsKey('${source.UUID}_layerStyle')) {
     //   buffer.write('${SN_UUIDtoVarName[source.UUID + '_layerStyle']} ?? ');
     // }
+
+    // TODO: do we need this?
+    var imagePath = source is InheritedBitmap
+        ? p.relative(source.referenceImage, from: MainInfo().outputPath)
+        : ('assets/images/' + source.UUID + '.png');
+
     buffer.write(
-        '\'assets/${source is InheritedBitmap ? source.referenceImage : ('images/' + source.UUID + '.png')}\', ${_sizehelper.generate(source, generatorContext)})');
+        '\'$imagePath\', ${_sizehelper.generate(source, generatorContext)})');
     return buffer.toString();
   }
 }
