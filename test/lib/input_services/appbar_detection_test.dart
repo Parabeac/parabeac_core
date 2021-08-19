47d6483056fb2899fff96be0e1c98dd794b58fff
Bryan Figueroa <bryan@parabeac.com>
2021-08-19 15:54:25 -0600

    Fixed app bar tests

--- test/lib/input_services/appbar_detection_test.dart
+++ test/lib/input_services/appbar_detection_test.dart
@@ -1,33 +1,38 @@
+import 'dart:math';
 import 'package:mockito/mockito.dart';
 import 'package:parabeac_core/eggs/injected_app_bar.dart';
-import 'package:parabeac_core/input/sketch/entities/layers/abstract_layer.dart';
-import 'package:parabeac_core/input/sketch/entities/objects/frame.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
-import 'package:parabeac_core/generation/generators/plugins/pb_plugin_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/pb_intermediate_node_tree.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_plugin_list_helper.dart';
 import 'package:test/test.dart';
 
-class SketchNodeMock extends Mock implements SketchNode {}
+class PBINodeMock extends Mock implements PBIntermediateNode {}
 
 class ContextMock extends Mock implements PBContext {}
 
+class MockPBITree extends Mock implements PBIntermediateTree {}
+
 void main() {
   group('Detecting appbar from semantic', () {
-    SketchNodeMock sketchNode;
+    PBINodeMock pbiNodeMock;
     PBPluginListHelper helper;
     ContextMock context;
+    MockPBITree mockTree;
     setUp(() {
       context = ContextMock();
       helper = PBPluginListHelper();
       helper.initPlugins(context);
-      sketchNode = SketchNodeMock();
-      when(sketchNode.name).thenReturn('element<navbar>');
-      when(sketchNode.boundaryRectangle)
-          .thenReturn(Frame(x: 0, y: 0, width: 300, height: 20));
+      pbiNodeMock = PBINodeMock();
+      mockTree = MockPBITree();
+      when(pbiNodeMock.name).thenReturn('element<navbar>');
+      when(pbiNodeMock.frame).thenReturn(Rectangle(0, 0, 300, 20));
+      when(mockTree.childrenOf(any))
+          .thenReturn(<PBIntermediateNode>[pbiNodeMock]);
     });
 
     test('Checking for appbar', () {
-      var pNode = helper.returnAllowListNodeIfExists(sketchNode);
+      var pNode = helper.returnAllowListNodeIfExists(pbiNodeMock, mockTree);
       expect(pNode.runtimeType, InjectedAppbar);
     });
   });
