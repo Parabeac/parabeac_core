e07592f8f3bb7d15f4210d031bcea17c8d51614c
Eddie Herrera <eherreraga@miners.utep.edu>
2021-08-26 20:46:11 -0600

    WIP CREATED InjectedCenter to center elements.

---
+++ lib/interpret_and_optimize/entities/alignments/injected_center.dart
@@ -0,0 +1,18 @@
+import 'package:parabeac_core/generation/generators/visual-widgets/pb_center_gen.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/interfaces/pb_injected_intermediate.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_constraints.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/align_strategy.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/child_strategy.dart';
+
+/// Represents the Center widget used in flutter to center its elements within
+class InjectedCenter extends PBIntermediateNode
+    implements PBInjectedIntermediate {
+  InjectedCenter(String UUID, Rectangle3D<num> frame, String name,
+      {PBIntermediateConstraints constraints})
+      : super(UUID, frame, name, constraints: constraints) {
+    generator = PBCenterGenerator();
+    childrenStrategy = OneChildStrategy('child');
+    alignStrategy = NoAlignment();
+  }
+}
