3462dfee13b49bfe381249a9620e4c30592dd5d5
Bryan Figueroa <bryan@parabeac.com>
2021-09-09 17:49:44 -0600

    Only process node if it is visible

--- lib/interpret_and_optimize/helpers/abstract_intermediate_node_factory.dart
+++ lib/interpret_and_optimize/helpers/abstract_intermediate_node_factory.dart
@@ -45,7 +45,7 @@ class AbstractIntermediateNodeFactory {
   static dynamic getIntermediateNode(Map<String, dynamic> json,
       PBIntermediateNode parent, PBIntermediateTree tree) {
     var className = json[INTERMEDIATE_TYPE];
-    if (className != null) {
+    if (className != null && json['isVisible']) {
       for (var candidate in _intermediateNodes) {
         if (candidate.type == className) {
           var iNode = candidate.createIntermediateNode(json, parent, tree);
