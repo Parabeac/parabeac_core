55c6a4a16f867d1ca6a5a1155bd9aa33110a230c
Bryan Figueroa <bryan@parabeac.com>
2021-10-12 16:39:00 -0600

    Cleaned imports

--- lib/generation/generators/attribute-helper/pb_size_helper.dart
+++ lib/generation/generators/attribute-helper/pb_size_helper.dart
@@ -1,5 +1,4 @@
 import 'package:parabeac_core/generation/generators/attribute-helper/pb_attribute_gen_helper.dart';
-import 'package:parabeac_core/interpret_and_optimize/entities/inherited_bitmap.dart';
 import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
 
