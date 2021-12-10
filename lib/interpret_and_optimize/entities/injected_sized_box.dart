09f64f1e87e34b782676229b90f9b372d0102d78
Bryan Figueroa <bryan@parabeac.com>
2021-12-09 19:25:28 -0700

    Created PBIntermediatedSizedBox class

---
+++ lib/interpret_and_optimize/entities/injected_sized_box.dart
@@ -0,0 +1,40 @@
+import 'package:parabeac_core/generation/generators/pb_generator.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_intermediate_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/entities/subclasses/pb_visual_intermediate_node.dart';
+import 'package:parabeac_core/interpret_and_optimize/helpers/pb_context.dart';
+
+class PBSizedBox extends PBVisualIntermediateNode {
+  num height, width;
+
+  PBSizedBox({
+    String UUID,
+    Rectangle3D<num> Rectangle3D,
+    String name,
+    this.width,
+    this.height,
+  }) : super(UUID, Rectangle3D, name) {
+    generator = PBSizedBoxGenerator();
+  }
+}
+
+class PBSizedBoxGenerator extends PBGenerator {
+  @override
+  String generate(PBIntermediateNode source, PBContext context) {
+    if (source is PBSizedBox) {
+      var buffer = StringBuffer();
+
+      buffer.write('SizedBox(');
+
+      if (source.height != null) {
+        buffer.write('height: ${source.height},');
+      }
+      if (source.width != null) {
+        buffer.write('width: ${source.width},');
+      }
+
+      buffer.write(')');
+
+      return buffer.toString();
+    }
+  }
+}
