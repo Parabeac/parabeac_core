6695d1b2c2b04f1ea96f5cb0f8da28b58ca85951
Bryan Figueroa <bryan@parabeac.com>
2022-02-07 13:51:46 -0700

    Added blended color

--- lib/interpret_and_optimize/state_management/intermediate_auxillary_data.dart
+++ lib/interpret_and_optimize/state_management/intermediate_auxillary_data.dart
@@ -27,26 +27,75 @@ class IntermediateAuxiliaryData {
   /// Style for text
   PBTextStyle intermediateTextStyle;
 
+  /// Blanded color
   @JsonKey(ignore: true)
-  PBColor get color => _getColor();
-
-  PBColor _getColor() {
-    if (colors != null && colors.isNotEmpty) {
-      return colors.first.color;
-    } else {
-      return null;
-    }
-  }
+  PBColor color;
 
   IntermediateAuxiliaryData({
     this.colors,
     this.borderInfo,
     this.effects,
     this.intermediateTextStyle,
-  });
+  }) {
+    color = _calculateBlendColor();
+  }
 
   factory IntermediateAuxiliaryData.fromJson(Map<String, dynamic> json) =>
       _$IntermediateAuxiliaryDataFromJson(json);
 
   Map<String, dynamic> toJson() => _$IntermediateAuxiliaryDataToJson(this);
+
+  // Calculate just one color from all fills
+  PBColor _calculateBlendColor() {
+    var tempColor = [];
+    if (colors.isNotEmpty) {
+      colors.forEach((fill) {
+        if (fill.type == 'SOLID') {
+          if (tempColor.isEmpty) {
+            tempColor = _colorToList(fill);
+          } else {
+            var temp = _colorToList(fill);
+            tempColor = _addColors(tempColor, temp);
+          }
+        }
+      });
+      return PBColor(tempColor[3], tempColor[0], tempColor[1], tempColor[2]);
+    } else {
+      return null;
+    }
+  }
+
+  // This method takes two PBDL Colors in list form and adds them
+  List<double> _addColors(List<double> base, List<double> added) {
+    var tempColor = <double>[];
+
+    var calculatedAlpha = 1 - (1 - added[3]) * (1 - base[3]);
+
+    tempColor.add(((added[0] / 2) + (base[0] / 2)).roundToDouble());
+    tempColor.add(((added[1] / 2) + (base[1] / 2)).roundToDouble());
+    tempColor.add(((added[2] / 2) + (base[2] / 2)).roundToDouble());
+    tempColor.add(calculatedAlpha);
+
+    return tempColor;
+  }
+
+  // Returns a pbdl fill to a list of double
+  List<double> _colorToList(PBFill fill) {
+    var alpha = _getAlpha(fill.opacity);
+    var temp = <double>[];
+    temp.add(fill.color.r);
+    temp.add(fill.color.g);
+    temp.add(fill.color.b);
+    temp.add(alpha);
+    return temp;
+  }
+
+  // Calculates the alpha of a fill
+  double _getAlpha(num opacity) {
+    if (opacity > 1) {
+      return (opacity / 100).toDouble();
+    } else {
+      return opacity.toDouble();
+    }
+  }
 }
