cc676658934bccdb4c99bbce628b48d0a1dd78a8
Bryan Figueroa <bryan@parabeac.com>
2022-02-18 12:32:57 -0700

    Added missing gradient information

--- lib/interpret_and_optimize/state_management/auxilary_data_helpers/intermediate_fill.dart
+++ lib/interpret_and_optimize/state_management/auxilary_data_helpers/intermediate_fill.dart
@@ -1,11 +1,15 @@
+import 'dart:math';
 import 'package:parabeac_core/interpret_and_optimize/helpers/pb_color.dart';
-import 'package:pbdl/src/pbdl/pbdl_color.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'intermediate_fill.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class PBFill {
+  List<PBGradientStop> gradientStops;
+  @JsonKey(fromJson: _pointsFromJson, toJson: _pointsToJson)
+  List<Point> gradientHandlePositions;
+
   // String that tidentifies the ID of the image
   String imageRef;
 
@@ -36,23 +40,38 @@ class PBFill {
   factory PBFill.fromJson(Map<String, dynamic> json) => _$PBFillFromJson(json);
 
   Map<String, dynamic> toJson() => _$PBFillToJson(this);
+
+  static List<Point> _pointsFromJson(List points) {
+    var objPoints = <Point>[];
+    for (var point in points) {
+      objPoints.add(Point(point['x'], point['y']));
+    }
+    return objPoints;
+  }
+
+  static List<Map> _pointsToJson(List<Point> points) {
+    var maps = <Map>[];
+    if (points != null) {
+      for (var p in points) {
+        maps.add({'x': p.x, 'y': p.y});
+      }
+    }
+    return maps;
+  }
 }
 
-// enum BlendMode {
-//   NORMAL,
-//   DARKEN,
-//   MULTIPLY,
-//   COLOR_BURN,
-//   LIGHTEN,
-//   SCREEN,
-//   COLOR_DODGE,
-//   OVERLAY,
-//   SOFT_LIGHT,
-//   HARD_LIGHT,
-//   DIFFERENCE,
-//   EXCLUSION,
-//   HUE,
-//   SATURATION,
-//   COLOR,
-//   LUMINOSITY,
-// }
+@JsonSerializable()
+class PBGradientStop {
+  PBColor color;
+  num position;
+
+  PBGradientStop({
+    this.color,
+    this.position,
+  });
+
+  factory PBGradientStop.fromJson(Map<String, dynamic> json) =>
+      _$PBGradientStopFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PBGradientStopToJson(this);
+}
--- lib/interpret_and_optimize/state_management/auxilary_data_helpers/intermediate_fill.g.dart
+++ lib/interpret_and_optimize/state_management/auxilary_data_helpers/intermediate_fill.g.dart
@@ -16,14 +16,40 @@ PBFill _$PBFillFromJson(Map<String, dynamic> json) {
         ? null
         : PBColor.fromJson(json['color'] as Map<String, dynamic>),
     imageRef: json['imageRef'] as String,
-  );
+  )
+    ..gradientStops = (json['gradientStops'] as List)
+        ?.map((e) => e == null
+            ? null
+            : PBGradientStop.fromJson(e as Map<String, dynamic>))
+        ?.toList()
+    ..gradientHandlePositions =
+        PBFill._pointsFromJson(json['gradientHandlePositions'] as List);
 }
 
 Map<String, dynamic> _$PBFillToJson(PBFill instance) => <String, dynamic>{
+      'gradientStops':
+          instance.gradientStops?.map((e) => e?.toJson())?.toList(),
+      'gradientHandlePositions':
+          PBFill._pointsToJson(instance.gradientHandlePositions),
       'imageRef': instance.imageRef,
       'color': instance.color?.toJson(),
       'opacity': instance.opacity,
       'blendMode': instance.blendMode,
       'type': instance.type,
       'isEnabled': instance.isEnabled,
+    };
+
+PBGradientStop _$PBGradientStopFromJson(Map<String, dynamic> json) {
+  return PBGradientStop(
+    color: json['color'] == null
+        ? null
+        : PBColor.fromJson(json['color'] as Map<String, dynamic>),
+    position: json['position'] as num,
+  );
+}
+
+Map<String, dynamic> _$PBGradientStopToJson(PBGradientStop instance) =>
+    <String, dynamic>{
+      'color': instance.color,
+      'position': instance.position,
     };
