006ee404b13f22378db8778878e8bd01a26c1636
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-03-12 21:15:52 -0600

    Edit header on [DashBookBook] class.
    This is for added clarity that DashbookBook is not actually a Dashbook-supported widget, but rather a way for Stories to be grouped and generated more conveniently.

--- lib/generation/flutter_project_builder/post_gen_tasks/comp_isolation/dashbook/entities/dashbook_book.dart
+++ lib/generation/flutter_project_builder/post_gen_tasks/comp_isolation/dashbook/entities/dashbook_book.dart
@@ -1,7 +1,11 @@
 import 'package:parabeac_core/generation/flutter_project_builder/post_gen_tasks/comp_isolation/dashbook/entities/dashbook_story.dart';
 import 'package:parabeac_core/generation/flutter_project_builder/post_gen_tasks/comp_isolation/isolation_node.dart';
 
-/// Class that represents a DashBook Book.
+/// Class that represents a group of [DashBookStories].
+/// 
+/// This class does not actually exist in Dashbook. However,
+/// it makes it easier for us to group stories and generate them
+/// in a single step.
 class DashBookBook extends IsolationNode {
   DashBookBook({
     String name = 'Parabeac-Generated',
