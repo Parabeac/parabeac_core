1854eba086972e194612ee042848e071f5fe6dc2
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-03-23 10:24:57 -0600

    Improvements and documentation on SentryService

--- lib/analytics/sentry_analytics_service.dart
+++ lib/analytics/sentry_analytics_service.dart
@@ -1,32 +1,59 @@
+import 'package:quick_log/quick_log.dart';
 import 'package:sentry/sentry.dart';
 
+/// Class that uses Sentry to log transactions.
+///
+/// These transactions are meant to track performance throughout parabeac_core.
 class SentryService {
+  static final _logger = Logger('$SentryService');
   static var transactions = <String, ISentrySpan>{};
 
-  static void startTransaction(String name, String operation,
+  /// Starts a main transaction.
+  ///
+  /// @id The unique name of the transaction.
+  /// @operation The operation that is being performed.
+  /// @description Optional description of the transaction.
+  static void startTransaction(String id, String operation,
       {String description}) {
-    final transaction =
-        Sentry.startTransaction(name, operation, description: description);
+    if (!transactions.containsKey(id)) {
+      final transaction =
+          Sentry.startTransaction(id, operation, description: description);
 
-    transactions[name] = transaction;
-    print('#################################');
-    print(transactions[name].startTimestamp);
-    print('#################################');
+      transactions[id] = transaction;
+    } else {
+      _logger
+          .error('Transaction $id already exists. Cannot start transaction.');
+    }
   }
 
+  /// Starts a child transaction from a parent transaction with a unique [id].
+  ///
+  /// @id The unique name of the `parent` transaction.
+  /// @operation The `unique` operation that is being performed by the `child`. This will be used to identify child transactions.
+  /// @description Optional description of the transaction.
   static void startChildTransactionFrom(String id, String operation,
       {String description}) {
-    final transaction =
-        transactions[id].startChild(operation, description: description);
+    if (transactions.containsKey(id) && !transactions.containsKey(operation)) {
+      final transaction =
+          transactions[id].startChild(operation, description: description);
 
-    transactions[operation] = transaction;
+      transactions[operation] = transaction;
+    } else {
+      _logger.error(
+          'Transaction \"$id\" does not exist or transaction \"$operation\" already exists. Cannot start child transaction.');
+    }
   }
 
-  static Future<void> finishTransaction(String name) async {
-    await transactions[name].finish();
-
-    print('#################################');
-    print(transactions[name].endTimestamp);
-    print('#################################');
+  /// Finishes transaction with a unique [id].
+  ///
+  /// @id The unique name of the transaction.
+  static Future<void> finishTransaction(String id) async {
+    if (transactions.containsKey(id)) {
+      await transactions[id].finish();
+      transactions.remove(id);
+    } else {
+      _logger.error(
+          'Transaction $id does not exist. Cannot finish transaction.');
+    }
   }
 }
