8b3cc5d48a5a39848ee956288512a267d3b252ad
Bryan Figueroa <bryan@parabeac.com>
2022-04-04 19:35:04 -0600

    Auto layout golden test

---
+++ test/golden/auto_layout_test.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
+
+void main() {
+  final projectName = 'golden_auto_layout_testing_project';
+  final basePath = path.join(path.current, 'test', 'golden');
+  final runtimeFilePath = path.join(basePath, projectName, 'lib');
+  final goldenFilesPath = path.join(basePath, 'golden_files', 'auto_layout');
+  group('Auto Layout Golden Test', () {
+    setUp(() async {
+      // Run Parabeac core to generate test file
+      await Process.run('dart', [
+        'parabeac.dart',
+        '-f',
+        'PxDC3cxF6tNWVZpWSTb7Hu',
+        '-k',
+        '346172-e6b93eec-364f-4baa-bee8-24bf1e4d26da',
+        '-n',
+        '$projectName',
+        '-o',
+        '$basePath'
+      ]);
+    });
+    test('Generating Auto Layout and Comparing Golden File', () async {
+      /// Fetch a list with all the names for Auto Layout Golden Files
+      final fileNames =
+          File(path.join(goldenFilesPath, 'auto_layout_file_names.txt'));
+
+      final goldenFiles = <File>[];
+      final runtimeFiles = <File>[];
+
+      var fileNamesLines = fileNames.readAsLinesSync();
+
+      /// Add all files as golden files to the list
+      /// Also add run time files to another list
+      for (var i = 0; i < fileNamesLines.length; i++) {
+        goldenFiles.add(
+            File(path.join(goldenFilesPath, '${fileNamesLines[i]}.golden')));
+
+        runtimeFiles.add(File(path.join(runtimeFilePath, 'screens',
+            'auto_layout_permutations', '${fileNamesLines[i]}.g.dart')));
+      }
+
+      /// Iterate through golden/runtime files and compare them
+      for (var i = 0; i < goldenFiles.length; i++) {
+        var goldenFile = goldenFiles[i];
+        var runtimeFile = runtimeFiles[i];
+        var goldenFileLines = goldenFile.readAsLinesSync();
+        var runtimeFileLines = runtimeFile.readAsLinesSync();
+
+        for (var j = 0; j < goldenFileLines.length; j++) {
+          expect(runtimeFileLines[j], goldenFileLines[j],
+              reason: 'File ${path.basename(goldenFile.path)} Line $j');
+        }
+      }
+    }, timeout: Timeout(Duration(minutes: 5)));
+
+    tearDown(() async {
+      // Remove temporary project
+      await Process.start('rm', ['-rf', path.join(basePath, projectName)]);
+    });
+  });
+}
