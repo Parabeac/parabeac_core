3338bc0a833ff3831eaa7eed53e7859f032d57e9
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-06-02 13:03:15 -0600

    Add Text Styles generation Improve the way styling tasks are being added

---
+++ lib/generation/flutter_project_builder/post_gen_tasks/global_styling/text_styles_post_gen_task.dart
@@ -0,0 +1,51 @@
+import 'package:parabeac_core/generation/flutter_project_builder/post_gen_tasks/post_gen_task.dart';
+import 'package:parabeac_core/generation/generators/value_objects/file_structure_strategy/commands/add_constant_command.dart';
+import 'package:parabeac_core/generation/generators/value_objects/file_structure_strategy/file_ownership_policy.dart';
+import 'package:parabeac_core/generation/generators/value_objects/generation_configuration/pb_generation_configuration.dart';
+import 'package:pbdl/pbdl.dart';
+import 'package:recase/recase.dart';
+import 'package:uuid/uuid.dart';
+
+/// Class that generates all global [TextStyles] and exports them to a file
+class TextStylesPostGenTask extends PostGenTask {
+  GenerationConfiguration generationConfiguration;
+  List<PBDLGlobalTextStyle> textStyles;
+
+  TextStylesPostGenTask(this.generationConfiguration, this.textStyles);
+
+  @override
+  void execute() {
+    var constTextStyles = <ConstantHolder>[];
+
+    /// Format text styles to be added to constants file
+    textStyles.forEach((globalTextStyle) {
+      constTextStyles.add(ConstantHolder(
+        'TextStyle',
+        globalTextStyle.name.camelCase,
+        _textStyleStr(globalTextStyle.textStyle),
+      ));
+    });
+
+    generationConfiguration.fileStructureStrategy.commandCreated(
+      WriteConstantsCommand(
+        Uuid().v4(),
+        constTextStyles,
+        filename: 'text_styles',
+        ownershipPolicy: FileOwnership.PBC,
+        imports: 'import \'package:flutter/material.dart\';',
+      ),
+    );
+  }
+
+  String _textStyleStr(PBDLTextStyle textStyle) {
+    return '''
+TextStyle(
+  fontSize: ${textStyle.fontSize},
+  fontWeight: FontWeight.w${textStyle.fontWeight},
+  letterSpacing: ${textStyle.letterSpacing},
+  fontFamily: \'${textStyle.fontFamily}\',
+  color: Color(${textStyle.fills.first.color.toHex()}),
+)
+''';
+  }
+}
