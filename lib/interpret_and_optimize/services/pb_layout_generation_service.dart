b9767b05aa0ac1de8362db46e0e961405fd498a4
Bryan Figueroa <bryan@parabeac.com>
2022-06-24 15:52:57 -0600

    Corrected layoutProperty

--- lib/interpret_and_optimize/services/pb_layout_generation_service.dart
+++ lib/interpret_and_optimize/services/pb_layout_generation_service.dart
@@ -156,7 +156,7 @@ class PBLayoutGenerationService extends AITHandler {
             constraints: tempLayout.constraints.copyWith(),
             // Let the Container know if it is going to show
             // the width or height on the generation process
-            showHeight: tempLayout.layoutProperties.primaryAxisAlignment ==
+            showHeight: tempLayout.layoutProperties.primaryAxisSizing ==
                 IntermediateAxisMode.FIXED,
             showWidth: tempLayout.layoutProperties.crossAxisSizing ==
                 IntermediateAxisMode.FIXED,
