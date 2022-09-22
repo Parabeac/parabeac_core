ca9b82b403a41ece73f944c1e801353856d481f7
Bryan Figueroa <bryan@parabeac.com>
2022-09-22 17:47:04 -0600

    Created gradient code generation

--- lib/interpret_and_optimize/state_management/auxilary_data_helpers/intermediate_fill.dart
+++ lib/interpret_and_optimize/state_management/auxilary_data_helpers/intermediate_fill.dart
@@ -66,13 +66,58 @@ class PBFill {
     return maps;
   }
 
-  String constantGenerator() {
+  String initializerGenerator() {
     if (imageRef != null) {
       return 'Image.asset(\'$imageRef\')';
     } else if (type.toLowerCase().contains('gradient')) {
-      return 'Gradient()';
+      return _interpretGradient();
     }
     return '//TODO';
+  }
+
+  String _interpretGradient() {
+    if (type.toLowerCase().contains('radial')) {
+      print('radial');
+      return '''
+RadialGradient(
+  center: Alignment(0.0, 0.0),
+  ${_getColorsAndStops(gradientStops)}
+)
+        ''';
+    } else if (type.toLowerCase().contains('linear')) {
+      print('linear');
+      return ''' 
+LinearGradient(
+  begin: Alignment(${gradientHandlePositions[0].x}, ${gradientHandlePositions[0].y}),
+  end: Alignment(${gradientHandlePositions[1].x}, ${gradientHandlePositions[1].y}),
+   ${_getColorsAndStops(gradientStops)}
+)
+''';
+      //
+    } else if (type.toLowerCase().contains('angular')) {
+      // TODO: add support for angular gradient
+      return 'SweepGradient(colors: []) // We will support it in the future';
+    } else {
+      // Empty gradient TODO:
+      return 'null /* Not supported gradient */';
+    }
+  }
+
+  String _getColorsAndStops(List<PBGradientStop> gradientStops) {
+    var colors = '';
+    var stops = '';
+    for (var stop in gradientStops) {
+      colors += 'Color(' + stop.color.toString() + '),';
+      stops += stop.position.toString() + ',';
+    }
+    return '''
+colors: <Color>[
+    $colors
+  ],
+  stops: <double> [
+    $stops
+  ],
+''';
   }
 }
 
